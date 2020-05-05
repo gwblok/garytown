a763cf3edcecca29b5b311e79a724ec3c1fff87c
gwblok <garywblok@gmail.com>
2020-05-05 09:38:25 -0500

    Update o365_Content_DetectionMethod.ps1

--- Office365/o365_Content_DetectionMethod.ps1
+++ Office365/o365_Content_DetectionMethod.ps1
@@ -1,7 +1,7 @@
 #This is the detection Method for the Content Application
 #It looks for the Application Name, then checks to confirm it is in the CCM Cache, and that setup.exe is in the O365_Cache folder.
 
-$O365ContentAssignmentName = "Office 365 ProPlus Content"
+$O365ContentAssignmentName = "Office 365 ProPlus Content" #This is the App Deployment Type Name, not the actual app Name
 $O365_CacheLocation = "$env:ProgramData\O365_Cache\setup.exe"
 $CIModel = Get-CimInstance -Namespace root/ccm/CIModels -ClassName CCM_AppDeliveryTypeSynclet | Where-Object {$_.AppDeliveryTypeName -match $O365ContentAssignmentName}
 $ContentID = $CIModel.InstallAction.Content.ContentId | Sort-Object -Unique
