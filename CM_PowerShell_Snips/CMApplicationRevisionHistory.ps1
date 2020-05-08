4bb64f4849ec4bf49b4e9c7142e86f03462bdc8a
gwblok <garywblok@gmail.com>
2020-05-08 01:30:25 -0500

    Update CMApplicationRevisionHistory.ps1

--- CM_PowerShell_Snips/CMApplicationRevisionHistory.ps1
+++ CM_PowerShell_Snips/CMApplicationRevisionHistory.ps1
@@ -1,5 +1,5 @@
 ########################################################################################################################
-#Finds all Apps with "Microsoft Office 365" in the name, and removes all of teh revision history (besides the latest)
+#Finds all Apps with "Microsoft Office 365" in the name, and removes all of the revision history (besides the latest)
 $OfficeApps = Get-CMApplication | Where-Object {$_.LocalizedDisplayName -match "Microsoft Office 365"}
 
 foreach ($OfficeApp in $OfficeApps)
