719bbd6944cf6d6c914797dcc9de88185fa3d9cf
gwblok <garywblok@gmail.com>
2020-05-12 23:19:59 -0500

    Update o365_Change_Channel_ConfigItem.ps1

--- Office365/o365_Change_Channel_ConfigItem.ps1
+++ Office365/o365_Change_Channel_ConfigItem.ps1
@@ -5,40 +5,27 @@ Gary Blok @gwblok - GARYTOWN.COM
 ConfigItem Detection: Get-WmiObject -Namespace 'root\cimv2\sms' -Query "SELECT ProductName,ProductVersion FROM SMS_InstalledSoftware where ARPDisplayName like 'Microsoft Office 365 ProPlus%'"
 
 Use Same Script for Discovery & Remediation, just change $RunMode
-Requires Collection Variables on the Collections you run the Baseline - Looks up the Name of the Collection Variable
-Set the Names for the variables like so:
- - OfficeChannel-Monthly
- - OfficeChannel-Broad
- - OfficeChannel-Targeted
-The values you place in the variables don't get used.
-The script will look at those names and set the Office Channel to the Channel the variable is "named"
-
-OR...
 
 Set the $TargetChannelName to Broad / Monthly / Targeted and create 3 Baselines deployed to the 3 collections.  No need to bother with Collection Variables.
 #>
 $RunMode = "Remediate"
-#$TargetChannelName = "Monthly"  #Used for testing
-$namespace = "ROOT\ccm\Policy\Machine\ActualConfig"
-$classname = "CCM_CollectionVariable"
-$TargetChannelName = ((Get-CimInstance -Namespace $namespace -ClassName $classname | Where-Object {$_.Name -match "OfficeChannel"}).Name).split("-") | Where-Object {$_ -notmatch "OfficeChannel"}
+$TargetChannelName = "MonthlyEnterprise" 
+
 
 #Get Information about how Office is currently setup.
 $Configuration = "HKLM:\SOFTWARE\Microsoft\Office\ClickToRun\Configuration"
 $CurrentCDNBaseUrlValue = (Get-ItemProperty $Configuration).CDNBaseUrl
 $CurrentUpdateChannelValue = (Get-ItemProperty $Configuration).UpdateChannel
-$Insiders = "http://officecdn.microsoft.com/pr/64256afe-f5d9-4f86-8936-8840a6a4f5be"
-$Monthly = "http://officecdn.microsoft.com/pr/492350f6-3a01-4f97-b9c0-c7c6ddf67d60"
-$Targeted = "http://officecdn.microsoft.com/pr/b8f9b850-328d-4355-9145-c59439a0c4cf"
-$Broad = "http://officecdn.microsoft.com/pr/7ffbc6bf-bc32-4f92-8982-f9dd17fd3114"
-if ($CurrentCDNBaseUrlValue -eq $Insiders){$CurrentCDNBaseUrlName = "Insiders"}
-if ($CurrentCDNBaseUrlValue -eq $Monthly){$CurrentCDNBaseUrlName = "Monthly"}
-if ($CurrentCDNBaseUrlValue -eq $Targeted){$CurrentCDNBaseUrlName = "Targeted"}
-if ($CurrentCDNBaseUrlValue -eq $Broad){$CurrentCDNBaseUrlName = "Broad"}
-if ($TargetChannelName -eq "Insiders"){$TargetChannelValue = $Insiders}
-if ($TargetChannelName -eq "Monthly"){$TargetChannelValue = $Monthly}
-if ($TargetChannelName -eq "Targeted"){$TargetChannelValue = $Targeted}
-if ($TargetChannelName -eq "Broad"){$TargetChannelValue = $Broad}
+$CurrentPreview = "http://officecdn.microsoft.com/pr/64256afe-f5d9-4f86-8936-8840a6a4f5be"
+$Current = "http://officecdn.microsoft.com/pr/492350f6-3a01-4f97-b9c0-c7c6ddf67d60"
+$MonthlyEnterprise = "http://officecdn.microsoft.com/pr/55336b82-a18d-4dd6-b5f6-9e5095c314a6"
+$SemiAnnualPreview = "http://officecdn.microsoft.com/pr/b8f9b850-328d-4355-9145-c59439a0c4cf"
+$SemiAnnual = "http://officecdn.microsoft.com/pr/7ffbc6bf-bc32-4f92-8982-f9dd17fd3114"
+if ($CurrentCDNBaseUrlValue -eq $CurrentPreview){$CurrentCDNBaseUrlName = "CurrentPreview"}
+if ($CurrentCDNBaseUrlValue -eq $Current){$CurrentCDNBaseUrlName = "Current"}
+if ($CurrentCDNBaseUrlValue -eq $MonthlyEnterprise){$CurrentCDNBaseUrlName = "MonthlyEnterprise"}
+if ($CurrentCDNBaseUrlValue -eq $SemiAnnualPreview){$CurrentCDNBaseUrlName = "SemiAnnualPreview"}
+if ($CurrentCDNBaseUrlValue -eq $SemiAnnual){$CurrentCDNBaseUrlName = "SemiAnnual"}
 
 #Checks both Registry Keys, if even one of the two are not set to the correct Channel, it will run the remdiation (if set to remediation) otherwise Discovery will return non-compliant status.
 if ($CurrentUpdateChannelValue -ne $TargetChannelValue -or $CurrentCDNBaseUrlValue -ne $TargetChannelValue)
@@ -70,4 +57,3 @@ if ($CurrentUpdateChannelValue -ne $TargetChannelValue -or $CurrentCDNBaseUrlVal
     }
 else {Write-Output "Compliant"}
    
-
