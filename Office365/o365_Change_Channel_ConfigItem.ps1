e6d13579a409cd5180637d7b0ef859b49aae89ff
gwblok <garywblok@gmail.com>
2020-05-19 16:14:32 -0500

    Update o365_Change_Channel_ConfigItem.ps1

--- Office365/o365_Change_Channel_ConfigItem.ps1
+++ Office365/o365_Change_Channel_ConfigItem.ps1
@@ -7,25 +7,37 @@ ConfigItem Detection: Get-WmiObject -Namespace 'root\cimv2\sms' -Query "SELECT P
 Use Same Script for Discovery & Remediation, just change $RunMode
 
 Set the $TargetChannelName to Broad / Monthly / Targeted and create 3 Baselines deployed to the 3 collections.  No need to bother with Collection Variables.
+
+Change Log:
+2020.05.19 - Fixed Huge issue where this didn't work at all, how did I miss that? (Wasn't setting the $TargetChannelValue variable)
+
 #>
 $RunMode = "Remediate"
-$TargetChannelName = "MonthlyEnterprise" 
+$TargetChannelName = "MonthlyEnterprise"
 
 
 #Get Information about how Office is currently setup.
 $Configuration = "HKLM:\SOFTWARE\Microsoft\Office\ClickToRun\Configuration"
 $CurrentCDNBaseUrlValue = (Get-ItemProperty $Configuration).CDNBaseUrl
 $CurrentUpdateChannelValue = (Get-ItemProperty $Configuration).UpdateChannel
+#Define Channel Info
 $CurrentPreview = "http://officecdn.microsoft.com/pr/64256afe-f5d9-4f86-8936-8840a6a4f5be"
 $Current = "http://officecdn.microsoft.com/pr/492350f6-3a01-4f97-b9c0-c7c6ddf67d60"
 $MonthlyEnterprise = "http://officecdn.microsoft.com/pr/55336b82-a18d-4dd6-b5f6-9e5095c314a6"
 $SemiAnnualPreview = "http://officecdn.microsoft.com/pr/b8f9b850-328d-4355-9145-c59439a0c4cf"
 $SemiAnnual = "http://officecdn.microsoft.com/pr/7ffbc6bf-bc32-4f92-8982-f9dd17fd3114"
+#Capture Current Info for Reporting
 if ($CurrentCDNBaseUrlValue -eq $CurrentPreview){$CurrentCDNBaseUrlName = "CurrentPreview"}
 if ($CurrentCDNBaseUrlValue -eq $Current){$CurrentCDNBaseUrlName = "Current"}
 if ($CurrentCDNBaseUrlValue -eq $MonthlyEnterprise){$CurrentCDNBaseUrlName = "MonthlyEnterprise"}
 if ($CurrentCDNBaseUrlValue -eq $SemiAnnualPreview){$CurrentCDNBaseUrlName = "SemiAnnualPreview"}
 if ($CurrentCDNBaseUrlValue -eq $SemiAnnual){$CurrentCDNBaseUrlName = "SemiAnnual"}
+#Set Target Values to be used in Remediation
+if ($TargetChannelName -eq "CurrentPreview"){$TargetChannelValue = $CurrentPreview}
+if ($TargetChannelName -eq "Current"){$TargetChannelValue = $Current}
+if ($TargetChannelName -eq "MonthlyEnterprise"){$TargetChannelValue = $MonthlyEnterprise}
+if ($TargetChannelName -eq "SemiAnnualPreview"){$TargetChannelValue = $SemiAnnualPreview}
+if ($TargetChannelName -eq "SemiAnnual"){$TargetChannelValue = $SemiAnnual}
 
 #Checks both Registry Keys, if even one of the two are not set to the correct Channel, it will run the remdiation (if set to remediation) otherwise Discovery will return non-compliant status.
 if ($CurrentUpdateChannelValue -ne $TargetChannelValue -or $CurrentCDNBaseUrlValue -ne $TargetChannelValue)
