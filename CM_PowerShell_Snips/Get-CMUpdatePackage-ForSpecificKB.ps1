c824dd9a4bfdb98a7fda585c66c3b1e81c2894ab
gwblok <garywblok@gmail.com>
2020-12-05 11:11:19 -0600

    Update Get-CMUpdatePackage-ForSpecificKB.ps1

--- CM_PowerShell_Snips/Get-CMUpdatePackage-ForSpecificKB.ps1
+++ CM_PowerShell_Snips/Get-CMUpdatePackage-ForSpecificKB.ps1
@@ -1,5 +1,8 @@
-#@gwblok - Lookup KB and find the Deployment Package it is in.
+#@gwblok - 2020.12.05
 #Run on CM Provider Server, or add -computername and your CM Provider
+#Put in a KB and get the Upgrade Deployment Package
+#Only tested in my lab on a couple KBs
+#Might need to tweak to work, honestly don't know.
 
 $SiteCode = "PS2"
 $KB = "KB4586863"
@@ -9,4 +12,6 @@ $PKGC = Get-CimInstance -Namespace root/SMS/site_$($SiteCode) -ClassName SMS_Pac
 $Update = $UPD | Where-Object {$_.LocalizedDisplayName -match $KB}
 $UpdateInPackageID = (($PKGC | Where-Object {$_.ContentID -in $update.CI_ID}) | Select-Object -Property "PackageID" -Unique).PackageID
 $UpdateInPackage = $PKG | Where-Object {$_.PackageID -eq $UpdateInPackageID}
-$UpdateInPackage.Name
+Write-Output "Searched for $KB"
+Write-Output " Found in Software Update Package: $($UpdateInPackage.Name)"
+Write-Output "  $($UpdateInPackage.PkgSourcePath)"
