d8fce8e56163840cd2240c30f51def8c41209389
gwblok <garywblok@gmail.com>
2021-06-24 15:01:14 -0500

    Update CMApplicationRevisionHistory.ps1

--- CM_PowerShell_Snips/CMApplicationRevisionHistory.ps1
+++ CM_PowerShell_Snips/CMApplicationRevisionHistory.ps1
@@ -7,14 +7,21 @@ foreach ($OfficeApp in $OfficeApps)
     Get-CMApplicationRevisionHistory -InputObject $OfficeApp | Where-Object {$_.IsLatest -eq $false} | Remove-CMApplicationRevisionHistory -Force
     }
     
-########################################################################################################################
-#Removes Revision history from ALL Applications.  Be Careful!
-$AllApps = Get-CMApplication
 
-foreach ($App in $AllApps)
-    {
-    Write-Output $App.LocalizedDisplayName
-    Get-CMApplicationRevisionHistory -InputObject $OfficeApp | Where-Object {$_.IsLatest -eq $false} | Remove-CMApplicationRevisionHistory -Force
-    }
-    
+
 ########################################################################################################################
+#Finds all Apps with "Edge" in the name, and removes all of the revision history (besides the latest)
+$Apps = Get-CMApplication | Where-Object {$_.LocalizedDisplayName -match "Edge"}
+
+foreach ($App in $Apps)
+    {
+    $RevHistory = Get-CMApplicationRevisionHistory -InputObject $App
+    Write-host "Starting App: $($App.LocalizedDisplayName) | Has $($RevHistory.count) revisions" -ForegroundColor Magenta
+    Get-CMApplicationRevisionHistory -InputObject $App | Where-Object {$_.IsLatest -eq $false} | Remove-CMApplicationRevisionHistory -Force
+
+    #Confirm
+    $RevHistory = Get-CMApplicationRevisionHistory -InputObject $App
+    Write-host " Starting App: $($App.LocalizedDisplayName) now has $($RevHistory.count) revision from $($RevHistory.DateLastModified)" -ForegroundColor Green
+    write-host "-------------------------------------------------" -ForegroundColor Gray
+    } 
+
