327405c98701b414c76f88b6085a73919021031e
gwblok <garywblok@gmail.com>
2023-05-22 13:50:48 -0500

    Generalize Cod

--- Intune/Install-WMIExplorer-Detect.ps1
+++ Intune/Install-WMIExplorer-Detect.ps1
@@ -6,16 +6,17 @@ Gary Blok - @gwblok - GARYTOWN.COM
  .Description
   Downloads WMIExplorer from GitHub, Copies to System32 if it's not already there
 #>
-
+$AppName = "WMIExplorer"
 $FileName = "WMIExplorer.zip"
 $ExpandPath = "$env:windir\system32"
 $URL = "https://github.com/vinaypamnani/wmie2/releases/download/v2.0.0.2/WmiExplorer_2.0.0.2.zip"
+$AppPath = "$ExpandPath\WMIExplorer.exe"
 
-$WMIExplorerPath = "$ExpandPath\WMIExplorer.exe"
-if (!(Test-Path -Path $WMIExplorerPath)){
-    Write-Output "WMI Explorer Not Found, Exit 1"
+
+if (!(Test-Path -Path $AppPath)){
+    Write-Output "$AppName Not Found, Exit 1"
     exit 1
 }
 else {
-    Write-Output "WMI Explorer Already Installed"
+    Write-Output "$AppName Already Installed"
 }
--- Intune/Install-WMIExplorer-Remediate.ps1
+++ Intune/Install-WMIExplorer-Remediate.ps1
@@ -9,7 +9,43 @@ Gary Blok - @gwblok - GARYTOWN.COM
 #>
 
 
+#><#
+<#
+Gary Blok - @gwblok - GARYTOWN.COM
+.Synopsis
+  Proactive Remediation for WMIExplorer to be on endpoint
+
+ .Description
+  Downloads WMIExplorer from GitHub, Copies to System32 if it's not already there
 #>
+
+
+#>
+$AppName = "WMIExplorer"
+$FileName = "WMIExplorer.zip"
+$ExpandPath = "$env:windir\system32"
+$URL = "https://github.com/vinaypamnani/wmie2/releases/download/v2.0.0.2/WmiExplorer_2.0.0.2.zip"
+$AppPath = "$ExpandPath\WMIExplorer.exe"
+
+if (!(Test-Path -Path $AppPath)){
+    Write-Output "$AppName Not Found, Starting Remediation"
+    #Download & Extract to System32
+  Write-Output "Downloading $URL"
+  Invoke-WebRequest -UseBasicParsing -Uri $URL -OutFile $env:TEMP\$FileName
+  if (Test-Path -Path $env:TEMP\$FileName){Write-Output "Successfully Downloaded"}
+  else{Write-Output "Failed Downloaded"; exit 255}
+  Write-Output "Starting Extraction of $AppName to $ExpandPath"
+  Expand-Archive -Path $env:TEMP\$FileName -DestinationPath $ExpandPath -Force
+  if (Test-Path -Path $AppPath){Write-Output "Successfully Extracted Zip File"}
+  else{Write-Output "Failed Extract"; exit 255}
+}
+else {
+    Write-Output "$AppName Already Installed"
+}
+
+
+
+
 
 $FileName = "WMIExplorer.zip"
 $ExpandPath = "$env:windir\system32"
