aea9a1210cf817bc308b7fed5cd8dd75d1cb3031
gwblok <garywblok@gmail.com>
2023-06-02 13:10:12 -0500

    Create New-AppShortcut.ps1

---
+++ CM_PowerShell_Snips/New-AppShortcut.ps1
@@ -0,0 +1,49 @@
+Function New-AppShortcut {
+    <# Gary Blok | @gwblok | GARYTOWN.COM | 2023.06.02
+    Creates a shortcut for the exe you provide
+
+    
+    ex: New-AppShortcut -SourceExePath "C:\Windows\ccm\ClientUX\SCClient.exe" -ShortCutName "Software Center" -Desktop
+    That will create a shortcut for C:\Windows\ccm\ClientUX\SCClient.exe named Software Center and place it on the Desktop
+
+
+    ex: New-AppShortcut -SourceExePath "C:\Windows\system32\wmiexplorer.exe" -ShortCutName "WMIExplorer"
+    That will create a shortcut for C:\Windows\system32\wmiexplorer.exe named WMIExplorer and place it in the Start Menu
+
+    #>
+    
+    param(
+    [string]$SourceExePath = "$env:windir\system32\control.exe",
+    [string]$ArgumentsToSourceExe,
+    [string]$ShortCutName = "AppName",
+    [switch]$Desktop
+
+    )
+    #Build ShortCut Information
+    if ($Desktop){
+        $ShortCutFolderPath = "$env:Public\Desktop" 
+    }
+    else {
+        $ShortCutFolderPath = "$env:ProgramData\Microsoft\Windows\Start Menu\Programs"
+    }
+    $DestinationPath = "$ShortCutFolderPath\$($ShortCutName).lnk"
+    Write-Output "Shortcut Creation Path: $DestinationPath"
+
+    if ($ArgumentsToSourceExe){
+        Write-Output "Shortcut = $SourceExePath -$($ArgumentsToSourceExe)"
+    }
+    Else {
+        Write-Output "Shortcut = $SourceExePath"
+    }
+    
+
+    #Create Shortcut
+    $WshShell = New-Object -comObject WScript.Shell
+    $Shortcut = $WshShell.CreateShortcut($DestinationPath)
+    $Shortcut.IconLocation = "$SourceExePath, 0"
+    $Shortcut.TargetPath = $SourceExePath
+    if ($ArgumentsToSourceExe){$Shortcut.Arguments = $ArgumentsToSourceExe}
+    $Shortcut.Save()
+
+    Write-Output "Shortcut Created"
+}
