ce4c55ad2290b8bb647bc2311c5f871b6a0bad10
gwblok <garywblok@gmail.com>
2023-09-22 20:28:47 -0500

    Update Functions.ps1

--- Dev/CloudScripts/Functions.ps1
+++ Dev/CloudScripts/Functions.ps1
@@ -11,7 +11,7 @@ Function Inject-Win11ReqBypassRegValues {
     }
     
     if ($WindowsPhase -eq 'WinPE'){
-        #Borrowed and then Modified from: https://github.com/JosephM101/Force-Windows-11-Install/blob/main/Win11-TPM-RegBypass.ps1
+        #Insipiration & Some code from: https://github.com/JosephM101/Force-Windows-11-Install/blob/main/Win11-TPM-RegBypass.ps1
     
         # Mount and edit the setup environment's registry
         $REG_System = "C:\Windows\System32\config\system"
@@ -37,7 +37,6 @@ Function Inject-Win11ReqBypassRegValues {
         Set-Location -Path $ScriptDir
         Start-Sleep 1
         reg unload $VirtualRegistryPath_SYSTEM
-        # Start-Sleep 1
     }
     else {
         if (!(Test-Path -Path HKLM:\SYSTEM\Setup\LabConfig)){
@@ -53,4 +52,55 @@ Function Inject-Win11ReqBypassRegValues {
         }
         New-ItemProperty -Path "HKLM:\SYSTEM\Setup\MoSetup" -Name "AllowUpgradesWithUnsupportedTPMOrCPU" -Value 1 -PropertyType DWORD -Force
     }
+}
+function Run-WindowsUpdate{
+    <# Control Windows Update via PowerShell
+    Gary Blok - GARYTOWN.COM
+    NOTE: I'm using this in a RUN SCRIPT, so I hav the Parameters set to STRING, and in the RUN SCRIPT, I Create a list of options (TRUE & FALSE).
+    In a normal script, you wouldn't do this... so modify for your deployment method.
+
+    This was also intended to be used with ConfigMgr, if you're not, feel free to remove the $CMReboot & Corrisponding Function
+
+    Installing Updates using this Method does NOT notify the user, and does NOT let the user know that updates need to be applied at the next reboot.  It's 100% hidden.
+
+    HResult Lookup: https://docs.microsoft.com/en-us/windows/win32/wua_sdk/wua-success-and-error-codes-
+
+    #>
+
+    $Results = @(
+    @{ ResultCode = '0'; Meaning = "Not Started"}
+    @{ ResultCode = '1'; Meaning = "In Progress"}
+    @{ ResultCode = '2'; Meaning = "Succeeded"}
+    @{ ResultCode = '3'; Meaning = "Succeeded With Errors"}
+    @{ ResultCode = '4'; Meaning = "Failed"}
+    @{ ResultCode = '5'; Meaning = "Aborted"}
+    @{ ResultCode = '6'; Meaning = "No Updates Found"}
+    )
+
+
+    $WUDownloader=(New-Object -ComObject Microsoft.Update.Session).CreateUpdateDownloader()
+    $WUInstaller=(New-Object -ComObject Microsoft.Update.Session).CreateUpdateInstaller()
+    $WUUpdates=New-Object -ComObject Microsoft.Update.UpdateColl
+    ((New-Object -ComObject Microsoft.Update.Session).CreateupdateSearcher().Search("IsInstalled=0 and Type='Software'")).Updates|%{
+        if(!$_.EulaAccepted){$_.EulaAccepted=$true}
+        if ($_.Title -notmatch "Preview"){[void]$WUUpdates.Add($_)}
+    }
+
+    if ($WUUpdates.Count -ge 1){
+        $WUInstaller.ForceQuiet=$true
+        $WUInstaller.Updates=$WUUpdates
+        $WUDownloader.Updates=$WUUpdates
+        $UpdateCount = $WUDownloader.Updates.count
+        if ($UpdateCount -ge 1){
+            $Download = $WUDownloader.Download()
+        }
+        $InstallUpdateCount = $WUInstaller.Updates.count
+        if ($InstallUpdateCount -ge 1){
+            $Install = $WUInstaller.Install()
+            $ResultMeaning = ($Results | Where-Object {$_.ResultCode -eq $Install.ResultCode}).Meaning
+            Write-Output $ResultMeaning
+            #if ($Install.RebootRequired -eq $true){Restart-ComputerCM}
+        } 
+    }
+    else {Write-Output "No Updates Found"} 
 }
