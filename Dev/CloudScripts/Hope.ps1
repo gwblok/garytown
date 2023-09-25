ba64fe05dd8b19a41be53df0337fbbf022401bce
gwblok <garywblok@gmail.com>
2023-09-25 15:59:32 -0500

    Update Hope.ps1

--- Dev/CloudScripts/Hope.ps1
+++ Dev/CloudScripts/Hope.ps1
@@ -8,32 +8,40 @@ Creates Setup Complete Files
 #>
 
 $ScriptName = 'hope.garytown.com'
-$ScriptVersion = '23.9.25.2'
+$ScriptVersion = '23.9.25.3'
 
 Write-Host -ForegroundColor Green "[+] $ScriptName $ScriptVersion ($WindowsPhase Phase)"
 
 #Load Functions
-Write-Host -ForegroundColor Yellow "Restart after Script Completes?"
-$Restart = Read-Host "y or n, then Enter"
+if ($env:SystemDrive -ne 'X:') {
+    Write-Host -ForegroundColor Yellow "Restart after Script Completes?"
+    $Restart = Read-Host "y or n, then Enter"
+}
 
 iex (irm functions.garytown.com)
 iex (irm functions.osdcloud.com)
 
-#Remove Personal Teams
-iex (irm https://raw.githubusercontent.com/suazione/CodeDump/main/Set-ConfigureChatAutoInstall.ps1)
+#WinPE Stuff
 
+
+#Non-WinPE
+if ($env:SystemDrive -ne 'X:') {
+    #Remove Personal Teams
+    iex (irm https://raw.githubusercontent.com/suazione/CodeDump/main/Set-ConfigureChatAutoInstall.ps1)
+    Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/gwblok/garytown/85ad154fa2964ea4757a458dc5c91aea5bf483c6/HopeForUsedComputers/Hope%20for%20Used%20Computers%20PDF.pdf" -OutFile "C:\Users\Public\Desktop\Hope For Used Computers.pdf" -Verbose
+    #Windows Updates
+    Update-DefenderStack
+    Run-WindowsUpdate
+    Run-WindowsUpdateDriver
+
+
+}
+
+#Both
 #Set Random Stuff
 Inject-Win11ReqBypassRegValues
 Set-TimeZoneFromIP
 Enable-AutoZimeZoneUpdate
-
-#Windows Updates
-Update-DefenderStack
-Run-WindowsUpdate
-Run-WindowsUpdateDriver
-
-Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/gwblok/garytown/85ad154fa2964ea4757a458dc5c91aea5bf483c6/HopeForUsedComputers/Hope%20for%20Used%20Computers%20PDF.pdf" -OutFile "C:\Users\Public\Desktop\Hope For Used Computers.pdf" -Verbose
-
 
 if ($Restart -eq "Y"){Restart-Computer}
 
