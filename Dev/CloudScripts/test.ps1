c265c36aa1fe01378b9e9466d76a876b397dc1ce
gwblok <garywblok@gmail.com>
2023-09-25 17:43:35 -0500

    Update test.ps1

--- Dev/CloudScripts/test.ps1
+++ Dev/CloudScripts/test.ps1
@@ -1,1 +1,17 @@
+$Global:MyOSDCloud = [ordered]@{
+        Restart = [bool]$False
+        RecoveryPartition = [bool]$True
+        SkipAllDiskSteps = [bool]$False
+        DriverPackName = "None"
+    }
 
+#Update Files in Module that have been updated since last PowerShell Gallery Build (Testing Only)
+$ModulePath = (Get-ChildItem -Path "$($Env:ProgramFiles)\WindowsPowerShell\Modules\osd" | Where-Object {$_.Attributes -match "Directory"} | select -Last 1).fullname
+import-module "$ModulePath/OSD.psd1" -Force
+
+#Launch OSDCloud
+Write-Host "Starting OSDCloud" -ForegroundColor Green
+Start-OSDCloud -OSName 'Windows 11 22H2 x64' -OSEdition Pro -OSActivation Retail -ZTI -OSLanguage en-us
+
+Write-Host "Complete OSDCloud" -ForegroundColor Green
+iex (irm hope.garytown.com)
