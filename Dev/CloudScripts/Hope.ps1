e88013e3d901b881a23ddf419cfa8b364911d455
gwblok <garywblok@gmail.com>
2023-09-25 13:47:38 -0500

    Update Hope.ps1

--- Dev/CloudScripts/Hope.ps1
+++ Dev/CloudScripts/Hope.ps1
@@ -8,8 +8,12 @@ Creates Setup Complete Files
 #>
 
 #Load Functions
+$Restart = Read-Host "Please Type Y if you would like to Restart After Process Completes, otherwise press any key"
+
 iex (irm funtions.garytown.com)
 iex (irm functions.osdcloud.com)
+
+
 
 #Set Random Stuff
 Inject-Win11ReqBypassRegValues
@@ -21,7 +25,7 @@ Update-DefenderStack
 Run-WindowsUpdate
 Run-WindowsUpdateDriver
 
-
+if ($Restart = "Y"){Restart-Computer}
 
 
 <# Future version of OSD Module
