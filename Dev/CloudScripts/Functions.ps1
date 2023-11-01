aea2412567093f38ba45be670e45ee535745ffb0
gwblok <garywblok@gmail.com>
2023-11-01 11:57:44 -0500

    Update Functions.ps1

--- Dev/CloudScripts/Functions.ps1
+++ Dev/CloudScripts/Functions.ps1
@@ -514,6 +514,21 @@ function Install-CMTrace {
     }
 
 }
+Write-Host -ForegroundColor Green "[+] Function Set-LatestUpdatesASAPEnabled"
+function Set-LatestUpdatesASAPEnabled {
+    Write-Host "Enable 'Get the latest updates as soon as they’re available' Reg Value" -ForegroundColor DarkGray
+    if ($env:SystemDrive -eq 'X:') {
+        $WindowsPhase = 'WinPE'
+    }
+    if ($WindowsPhase -eq 'WinPE'){
+        Invoke-Exe reg load HKLM\TempSOFTWARE "C:\Windows\System32\Config\SOFTWARE"
+        Invoke-Exe reg add HKLM\TempSOFTWARE\Microsoft\WindowsUpdate\UX\Settings /V IsContinuousInnovationOptedIn /T REG_DWORD /D 1 /F
+        Invoke-Exe reg unload HKLM\TempSOFTWARE
+    }
+    else {
+        Invoke-Exe reg add HKLM\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings /V IsContinuousInnovationOptedIn /T REG_DWORD /D 1 /F
+    }
+}
 Write-Host -ForegroundColor Green "[+] Function Set-APEnterprise"
 function Set-APEnterprise {
     Install-Nuget
