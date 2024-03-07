a522d59250e1cffcfba1945e89a95c72e29fa053
gwblok <garywblok@gmail.com>
2024-03-07 11:02:20 -0600

    Update Functions.ps1

--- Dev/CloudScripts/Functions.ps1
+++ Dev/CloudScripts/Functions.ps1
@@ -1,5 +1,5 @@
 $ScriptName = 'functions.garytown.com'
-$ScriptVersion = '24.3.7.1'
+$ScriptVersion = '24.3.7.2'
 Set-ExecutionPolicy Bypass -Force
 
 Write-Host -ForegroundColor Green "[+] $ScriptName $ScriptVersion"
@@ -253,114 +253,10 @@ Function Set-Win11ReqBypassRegValues {
 }
 
 Write-Host -ForegroundColor Green "[+] Function Start-WindowsUpdate"
-function Start-WindowsUpdate{
-    <# Control Windows Update via PowerShell
-    Gary Blok - GARYTOWN.COM
-    NOTE: I'm using this in a RUN SCRIPT, so I hav the Parameters set to STRING, and in the RUN SCRIPT, I Create a list of options (TRUE & FALSE).
-    In a normal script, you wouldn't do this... so modify for your deployment method.
-
-    This was also intended to be used with ConfigMgr, if you're not, feel free to remove the $CMReboot & Corrisponding Function
-
-    Installing Updates using this Method does NOT notify the user, and does NOT let the user know that updates need to be applied at the next reboot.  It's 100% hidden.
-
-    HResult Lookup: https://docs.microsoft.com/en-us/windows/win32/wua_sdk/wua-success-and-error-codes-
-
-    #>
-
-    $Results = @(
-    @{ ResultCode = '0'; Meaning = "Not Started"}
-    @{ ResultCode = '1'; Meaning = "In Progress"}
-    @{ ResultCode = '2'; Meaning = "Succeeded"}
-    @{ ResultCode = '3'; Meaning = "Succeeded With Errors"}
-    @{ ResultCode = '4'; Meaning = "Failed"}
-    @{ ResultCode = '5'; Meaning = "Aborted"}
-    @{ ResultCode = '6'; Meaning = "No Updates Found"}
-    )
-
-
-    $WUDownloader=(New-Object -ComObject Microsoft.Update.Session).CreateUpdateDownloader()
-    $WUInstaller=(New-Object -ComObject Microsoft.Update.Session).CreateUpdateInstaller()
-    $WUUpdates=New-Object -ComObject Microsoft.Update.UpdateColl
-    ((New-Object -ComObject Microsoft.Update.Session).CreateupdateSearcher().Search("IsInstalled=0 and Type='Software'")).Updates|%{
-        if(!$_.EulaAccepted){$_.EulaAccepted=$true}
-        if ($_.Title -notmatch "Preview"){[void]$WUUpdates.Add($_)}
-    }
-
-    if ($WUUpdates.Count -ge 1){
-        $WUInstaller.ForceQuiet=$true
-        $WUInstaller.Updates=$WUUpdates
-        $WUDownloader.Updates=$WUUpdates
-        $UpdateCount = $WUDownloader.Updates.count
-        if ($UpdateCount -ge 1){
-            Write-Output "Downloading $UpdateCount Updates"
-            foreach ($update in $WUInstaller.Updates){Write-Output "$($update.Title)"}
-            $Download = $WUDownloader.Download()
-        }
-        $InstallUpdateCount = $WUInstaller.Updates.count
-        if ($InstallUpdateCount -ge 1){
-            Write-Output "Installing $InstallUpdateCount Updates"
-            $Install = $WUInstaller.Install()
-            $ResultMeaning = ($Results | Where-Object {$_.ResultCode -eq $Install.ResultCode}).Meaning
-            Write-Output $ResultMeaning
-        } 
-    }
-    else {Write-Output "No Updates Found"} 
-}
+iex (irm https://raw.githubusercontent.com/OSDeploy/OSD/master/Public/OSDCloudTS/Start-WindowsUpdate.ps1)
 
 Write-Host -ForegroundColor Green "[+] Function Start-WindowsUpdateDriver"
-function Start-WindowsUpdateDriver{
-    <# Control Windows Update via PowerShell
-    Gary Blok - GARYTOWN.COM
-    NOTE: I'm using this in a RUN SCRIPT, so I hav the Parameters set to STRING, and in the RUN SCRIPT, I Create a list of options (TRUE & FALSE).
-    In a normal script, you wouldn't do this... so modify for your deployment method.
-
-    This was also intended to be used with ConfigMgr, if you're not, feel free to remove the $CMReboot & Corrisponding Function
-
-    Installing Updates using this Method does NOT notify the user, and does NOT let the user know that updates need to be applied at the next reboot.  It's 100% hidden.
-
-    HResult Lookup: https://docs.microsoft.com/en-us/windows/win32/wua_sdk/wua-success-and-error-codes-
-
-    #>
-
-    $Results = @(
-    @{ ResultCode = '0'; Meaning = "Not Started"}
-    @{ ResultCode = '1'; Meaning = "In Progress"}
-    @{ ResultCode = '2'; Meaning = "Succeeded"}
-    @{ ResultCode = '3'; Meaning = "Succeeded With Errors"}
-    @{ ResultCode = '4'; Meaning = "Failed"}
-    @{ ResultCode = '5'; Meaning = "Aborted"}
-    @{ ResultCode = '6'; Meaning = "No Updates Found"}
-    )
-
-
-    $WUDownloader=(New-Object -ComObject Microsoft.Update.Session).CreateUpdateDownloader()
-    $WUInstaller=(New-Object -ComObject Microsoft.Update.Session).CreateUpdateInstaller()
-    $WUUpdates=New-Object -ComObject Microsoft.Update.UpdateColl
-    ((New-Object -ComObject Microsoft.Update.Session).CreateupdateSearcher().Search("IsInstalled=0 and Type='Driver'")).Updates|%{
-        if(!$_.EulaAccepted){$_.EulaAccepted=$true}
-        if ($_.Title -notmatch "Preview"){[void]$WUUpdates.Add($_)}
-    }
-
-    if ($WUUpdates.Count -ge 1){
-        $WUInstaller.ForceQuiet=$true
-        $WUInstaller.Updates=$WUUpdates
-        $WUDownloader.Updates=$WUUpdates
-        $UpdateCount = $WUDownloader.Updates.count
-        if ($UpdateCount -ge 1){
-            Write-Output "Downloading $UpdateCount Updates"
-            foreach ($update in $WUInstaller.Updates){Write-Output "$($update.Title)"}
-            $Download = $WUDownloader.Download()
-        }
-        $InstallUpdateCount = $WUInstaller.Updates.count
-        if ($InstallUpdateCount -ge 1){
-            Write-Output "Installing $InstallUpdateCount Updates"
-            $Install = $WUInstaller.Install()
-            $ResultMeaning = ($Results | Where-Object {$_.ResultCode -eq $Install.ResultCode}).Meaning
-            Write-Output $ResultMeaning
-        } 
-    }
-    else {Write-Output "No Updates Found"} 
-}
+iex (irm https://raw.githubusercontent.com/OSDeploy/OSD/master/Public/OSDCloudTS/Start-WindowsUpdateDrivers.ps1)
 
 Write-Host -ForegroundColor Green "[+] Function Enable-AutoTimeZoneUpdate"
 Function Enable-AutoTimeZoneUpdate {
