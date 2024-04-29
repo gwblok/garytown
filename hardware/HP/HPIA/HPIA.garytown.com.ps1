060c98ce67b22d6477a5469b9fc4aefaacecfa8b
gwblok <garywblok@gmail.com>
2024-04-28 22:04:02 -0500

    MMS Demos

--- hardware/HP/HPIA/HPIA.garytown.com.ps1
+++ hardware/HP/HPIA/HPIA.garytown.com.ps1
@@ -7,13 +7,8 @@ Write-Host " [+] Get-HPIAXMLResult" -ForegroundColor Gray
 Write-Host " [+] Get-HPIAJSONResult" -ForegroundColor Gray
 iex (irm https://raw.githubusercontent.com/OSDeploy/OSD/master/Public/OSDCloudTS/Invoke-HPIA.ps1)
 
-
+Write-Host ""
 write-host "Demo Commands" -ForegroundColor Green
-Write-host ' cd c:\temp' -ForegroundColor Gray
-Write-host ' Get-HPDeviceDetails -like *"840 G10"*"' -ForegroundColor Gray
-Write-host ' Get-HPDeviceDetails -like *"800*G9"*' -ForegroundColor Gray
-Write-host ' $Platforms = @("8B41", "8AC3")' -ForegroundColor Gray
-Write-host ' $DPs = $Platforms | foreach {Get-SoftpaqList -Category Driverpack -Os win11 -OsVer 22H2 -Platform $_}' -ForegroundColor Gray
-Write-host ' $DPs | foreach {Get-Softpaq -Number $_.id -Extract}' -ForegroundColor Gray
-Write-host '' -ForegroundColor Gray
-Write-host '' -ForegroundColor Gray
+Write-Host " Get-MMSDemo1" -ForegroundColor Gray
+Write-Host " Get-MMSDemo2" -ForegroundColor Gray
+Write-Host " Get-MMSDemo3" -ForegroundColor Gray
