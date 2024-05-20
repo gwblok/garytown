33537083f5fece62a89843b74e0a3972c029adf1
gwblok <garywblok@gmail.com>
2024-05-20 13:49:29 -0500

    Update Hope.ps1

--- Dev/CloudScripts/Hope.ps1
+++ Dev/CloudScripts/Hope.ps1
@@ -87,7 +87,7 @@ if ($env:SystemDrive -ne 'X:') {
     Set-ExecutionPolicy Bypass -Force
 
     #Setup Post Actions Scheduled Task
-    iex (irm https://raw.githubusercontent.com/gwblok/garytown/master/Dev/CloudScripts/PostActionsTask.ps1)
+    iex (irm "https://raw.githubusercontent.com/gwblok/garytown/master/Dev/CloudScripts/PostActionsTask.ps1")
 
     #Add Functions
     iex (irm functions.garytown.com)
