fc1231ff196b190c6e2f264cc4964e385f80fd3f
gwblok <garywblok@gmail.com>
2024-06-10 16:35:46 -0500

    Update OOBE_DELL.ps1

--- Dev/CloudScripts/OOBE_DELL.ps1
+++ Dev/CloudScripts/OOBE_DELL.ps1
@@ -3,7 +3,7 @@ $ScriptVersion = '24.06.10.01'
 
 #region Initialize
 
-Write-Host "Running $ScriptName - $ScriptVersion" -ForegroundColor Green
+
 $Manufacturer = (Get-CimInstance -Class:Win32_ComputerSystem).Manufacturer
 $Model = (Get-CimInstance -Class:Win32_ComputerSystem).Model
 $SystemSKUNumber = (Get-CimInstance -ClassName Win32_ComputerSystem).SystemSKUNumber
@@ -11,6 +11,7 @@ if ($Manufacturer -match "Dell"){
     $Manufacturer = "Dell"
     $DellEnterprise = Test-DCUSupport
     if ($DellEnterprise -eq $true) {
+        Write-Host "Running $ScriptName - $ScriptVersion" -ForegroundColor Green
         Write-Host -ForegroundColor Green "Dell System Supports Dell Command Update"
         Write-Host -ForegroundColor Green " Enabling Dell Functions: https://raw.githubusercontent.com/OSDeploy/OSD/master/cloud/modules/devicesdell.psm1"
         Invoke-Expression (Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/OSDeploy/OSD/master/cloud/modules/devicesdell.psm1')
