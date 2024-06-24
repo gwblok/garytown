d09a797daeca5e87e26964979bcae5ca812765ab
gwblok <garywblok@gmail.com>
2024-06-24 17:30:37 -0500

    Update BasicBIOSSettingsHPCMSL.ps1

--- hardware/HP/CMSL/BasicBIOSSettingsHPCMSL.ps1
+++ hardware/HP/CMSL/BasicBIOSSettingsHPCMSL.ps1
@@ -21,4 +21,24 @@ else {
     #Write-Output "Using Blank Password"
 }
 
-Set-HPBIOSSettingValue -Name 'Fast Boot' -Value 'Enable' -Password $BIOSWD
+try {Set-HPBIOSSettingValue -Name 'Fast Boot'                                         -Value "Enable"                                        -Password "$BIOSWD"} catch {}
+try {Set-HPBIOSSettingValue -Name 'Deep S3'                                           -Value "Off"                                           -Password "$BIOSWD"} catch {}
+try {Set-HPBIOSSettingValue -Name 'Deep Sleep'                                        -Value "Off"                                           -Password "$BIOSWD"} catch {}
+try {Set-HPBIOSSettingValue -Name 'S4/S5 Max Power Savings'                           -Value "Disable"                                       -Password "$BIOSWD"} catch {}
+try {Set-HPBIOSSettingValue -Name 'Num Lock State at Power-On'                        -Value "On"                                            -Password "$BIOSWD"} catch {}
+try {Set-HPBIOSSettingValue -Name 'NumLock on at boot'                                -Value "Disable"                                       -Password "$BIOSWD"} catch {}
+try {Set-HPBIOSSettingValue -Name 'Numlock state at boot'                             -Value "On"                                            -Password "$BIOSWD"} catch {}
+try {Set-HPBIOSSettingValue -Name 'PXE Internal IPV4 NIC boot'                        -Value "Enable"                                        -Password "$BIOSWD"} catch {}
+try {Set-HPBIOSSettingValue -Name 'PXE Internal NIC boot'                             -Value "Enable"                                        -Password "$BIOSWD"} catch {}
+try {Set-HPBIOSSettingValue -Name 'Wake On LAN'                                       -Value "Boot to Hard Drive"                            -Password "$BIOSWD"} catch {}
+try {Set-HPBIOSSettingValue -Name 'TPM State'                                         -Value "Enable"                                        -Password "$BIOSWD"} catch {}
+try {Set-HPBIOSSettingValue -Name 'TPM Device'                                        -Value "Available"                                     -Password "$BIOSWD"} catch {}
+try {Set-HPBIOSSettingValue -Name 'TPM Activation Policy'                             -Value "No prompts"                                    -Password "$BIOSWD"} catch {}
+try {Set-HPBIOSSettingValue -Name 'Lock BIOS Version'                                 -Value "Disable"                                       -Password "$BIOSWD"} catch {}
+try {Set-HPBIOSSettingValue -Name 'Native OS Firmware Update Service'                 -Value "Enable"                                        -Password "$BIOSWD"} catch {}
+try {Set-HPBIOSSettingValue -Name 'Virtualization Technology (VTx)'                   -Value "Enable"                                        -Password "$BIOSWD"} catch {}
+try {Set-HPBIOSSettingValue -Name 'Virtualization Technology for Directed I/O (VTd)'  -Value "Enable"                                        -Password "$BIOSWD"} catch {}
+try {Set-HPBIOSSettingValue -Name 'SVM CPU Virtualization'                            -Value "Enable"                                        -Password "$BIOSWD"} catch {}
+try {Set-HPBIOSSettingValue -Name 'Secure Boot'                                       -Value "Enable"                                        -Password "$BIOSWD"} catch {}
+try {Set-HPBIOSSettingValue -Name 'UEFI Boot Options'                                 -Value "Enable"                                        -Password "$BIOSWD"} catch {}
+try {Set-HPBIOSSettingValue -Name 'Configure Legacy Support and Secure Boot'          -Value "Legacy Support Disable and Secure Boot Enable" -Password "$BIOSWD"} catch {}
