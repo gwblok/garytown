796ddaf45732a84eb08ca4ffe642ca1077cc9b49
Gary Blok <gary@thebloks.com>
2024-07-22 08:42:03 -0500

    24.7.22.1

--- hardware/Dell/CommandUpdate/CMSL/Get-DellSupportedModels.ps1
+++ hardware/Dell/CommandUpdate/CMSL/Get-DellSupportedModels.ps1
@@ -1,13 +1,13 @@
 function Get-DellSupportedModels {
     [CmdletBinding()]
     
-    $CabPathIndex = "$env:temp\DellCabDownloads\CatalogIndexPC.cab"
-    $DellCabExtractPath = "$env:temp\DellCabDownloads\DellCabExtract"
+    $CabPathIndex = "$env:ProgramData\CMSL\DellCabDownloads\CatalogIndexPC.cab"
+    $DellCabExtractPath = "$env:ProgramData\CMSL\DellCabDownloads\DellCabExtract"
 
     # Pull down Dell XML CAB used in Dell Command Update ,extract and Load
     if (!(Test-Path $DellCabExtractPath)){$null = New-Item -Path $DellCabExtractPath -ItemType Directory -Force}
     Write-Verbose "Downloading Dell Cab"
-    Invoke-WebRequest -Uri "https://downloads.dell.com/catalog/CatalogIndexPC.cab" -OutFile $CabPathIndex -UseBasicParsing -Verbose -Proxy $ProxyServer
+    Invoke-WebRequest -Uri "https://downloads.dell.com/catalog/CatalogIndexPC.cab" -OutFile $CabPathIndex -UseBasicParsing -Proxy $ProxyServer
     If(Test-Path "$DellCabExtractPath\DellSDPCatalogPC.xml"){Remove-Item -Path "$DellCabExtractPath\DellSDPCatalogPC.xml" -Force}
     Start-Sleep -Seconds 1
     if (test-path $DellCabExtractPath){Remove-Item -Path $DellCabExtractPath -Force -Recurse}
@@ -16,7 +16,7 @@ function Get-DellSupportedModels {
     $null = expand $CabPathIndex $DellCabExtractPath\CatalogIndexPC.xml
 
     Write-Verbose "Loading Dell Catalog XML.... can take awhile"
-    [xml]$XMLIndex = Get-Content "$DellCabExtractPath\CatalogIndexPC.xml" -Verbose
+    [xml]$XMLIndex = Get-Content "$DellCabExtractPath\CatalogIndexPC.xml"
 
 
     $SupportedModels = $XMLIndex.ManifestIndex.GroupManifest
@@ -30,4 +30,31 @@ function Get-DellSupportedModels {
         $SupportedModelsObject += $SPInventory 
     }
     return $SupportedModelsObject
+}
+
+Function Get-DCUVersion {
+    $DCU=(Get-ItemProperty "HKLM:\SOFTWARE\Dell\UpdateService\Clients\CommandUpdate\Preferences\Settings" -ErrorVariable err -ErrorAction SilentlyContinue)
+    if ($err.Count -eq 0) {
+        $DCU = $DCU.ProductVersion
+    }else{
+        $DCU = $false
+    }
+    return $DCU
+}
+
+Function Install-DCU {
+    
+    $temproot = "$env:windir\temp"
+    $LogFilePath = "$env:ProgramData\CMSL\Logs"
+    $LogFile = "$LogFilePath\DCU-Install.log"
+    $Manufacturer = (Get-CimInstance -ClassName Win32_ComputerSystem).Manufacturer
+    $CabPathIndex = "$temproot\DellCabDownloads\CatalogIndexPC.cab"
+    $CabPathIndexModel = "$temproot\DellCabDownloads\CatalogIndexModel.cab"
+    $DellCabExtractPath = "$temproot\DellCabDownloads\DellCabExtract"
+    $Compliance = $true
+    $Remediate = $true
+    $DCUVersion = Get-DCUVersion
+
+    $DellXML = Get-DellSupportedModels | Where-Object {$_.URL -match "Latitude"} |  Sort-Object -Descending -Property Date | Select-Object -first 1 
+
 }
