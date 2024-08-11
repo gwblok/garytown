52d18b61f3fec279b5612a513aa07b112aea4224
gwblok <garywblok@gmail.com>
2024-08-10 22:52:12 -0500

    Create Set-DiskNumber.ps1

---
+++ OSD/Set-DiskNumber.ps1
@@ -0,0 +1,13 @@
+#Script Returns the Disk Number for the Desired Disk for installing Windows to during OS
+#Get NVMe Disk
+$BusType = 'NVMe'
+$Disks = Get-Disk | Where-Object {$_.BusType -Match $BusType}
+#If Non-Found, just use the smallest disk size:
+if (!$disk){
+    $DiskNumber = (get-disk | Where-Object {$_.size -eq ((get-disk | Where-Object {$_.BusType -notmatch "USB"}).size | measure -Minimum).Minimum}).Number
+}
+#If found, get the smallest one (just incase there is more than one)
+else{
+    $DiskNumber = ($Disks | Where-Object {$_.size -eq ((get-disk | Where-Object {$_.BusType -match $BusType}).size | measure -Minimum).Minimum}).Number
+}
+return $DiskNumber
