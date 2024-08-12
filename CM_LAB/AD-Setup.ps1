c690bf2f2551190188f16d180f294c4b5b689674
gwblok <garywblok@gmail.com>
2024-08-12 15:25:53 -0500

    Update AD-Setup.ps1

--- CM_LAB/AD-Setup.ps1
+++ CM_LAB/AD-Setup.ps1
@@ -46,18 +46,31 @@ $ADGroups = @(
 @{Name = "CM App Deploy Users"; Description = "Group to Target Apps to in CM - Basically contains Domain Users"}
 @{Name = "Certificate Admins"; Description = "Allows user sto enrool in the Web Server Certs"}
 @{Name = "Web Server Cert Enrollment"; Description = "Group of CM Servers with IIS"}
+#2Pint Groups:
+@{Name = "StifleR Global Admins"; Description = "Full read and write right access to ALL objects"}
+@{Name = "StifleR Dashboard Access"; Description = "Statistics, summary data etc. No WMI access  "}
+@{Name = "StifleR Global Read"; Description = "Read Access on ALL locations. Must be member of Dashboard Access also "; Group = "StifleR Dashboard Access"}
 )
 
 $UsersGroupsOU = Get-ADOrganizationalUnit -Filter 'Name -eq "Users & Groups"'
 ForEach ($ADGroup in $ADGroups){
     $Name = $ADGroup.Name
+    $AddtoGroups = $ADGroup.Group
     if (!(Get-ADGroup -Filter "Name -eq '$Name'")){
         $Description = $ADGroup.Description
         New-ADGroup -Name $Name -Description $Description -GroupCategory Security -GroupScope Universal 
         $Group = Get-ADGroup -Identity $Name
         Move-ADObject -Identity $Group.DistinguishedName -TargetPath $UsersGroupsOU.DistinguishedName
+        $Group = Get-ADGroup -Identity $Name
         Write-Host "Created Group $Name" -ForegroundColor Green
+        if ($ADGroup.Group -ne $null){
+            ForEach ($AddtoGroup in $AddtoGroups){
+                $WorkingGroup = Get-ADGroup -Filter "Name -eq '$AddtoGroup'"
+                Add-ADGroupMember -Identity $WorkingGroup.DistinguishedName -Members $Group.DistinguishedName
+                Write-Host " Adding $Name to Group $AddtoGroup" -ForegroundColor DarkGray
+            }
         }
+    }
     else {Write-Host "Group $Name Already Exist" -ForegroundColor Yellow}
 }
 
@@ -71,12 +84,15 @@ $ADUsers = @(
 @{Name = "CM_NA"; GivenName = "ConfigMgr"; Surname = "Network Access" ; Description = "Network Access Account"; LogonWorkstations = "Null"}
 @{Name = "CM_CP_Servers"; GivenName = "ConfigMgr"; Surname = "ClientPush Servers" ; Description = "Client Push Servers"; Group = "Local Admins Servers"}
 @{Name = "CM_CP_Workstations"; GivenName = "ConfigMgr"; Surname = "ClientPush Workstations" ; Description = "Client Push Workstations"; Group = "Local Admins Workstations"}
-@{Name = "Gary.Blok"; GivenName = "Gary"; Surname = "Blok" ; Description = "ME"; Group = @("Local Admins Workstations","Domain Admins", "CM Full Admins","CM App Deploy Users")}
-@{Name = "Mark.Godfrey"; GivenName = "Mark"; Surname = "Godfrey" ; Description = "Friend"; Group = @("Local Admins Workstations", "CM Full Admins","CM App Deploy Users")}
-@{Name = "David.Segura"; GivenName = "David"; Surname = "Segura" ; Description = "Friend"; Group = @("Local Admins Workstations","CM App Deploy Users")}
-@{Name = "Nathan.Ziehnert"; GivenName = "Nathan"; Surname = "Ziehnert" ; Description = "Friend"; Group = @("Domain Admins", "CM Full Admins","CM App Deploy Users")}
-@{Name = "Troy.Martin"; GivenName = "Troy"; Surname = "Martin" ; Description = "Friend"; Group = @("CM Full Admins","CM App Deploy Users")}
-@{Name = "Mike.Terrill"; GivenName = "Mike"; Surname = "Terrill" ; Description = "Friend"; Group = @("Local Admins Workstations","CM Full Admins","CM App Deploy Users")}
+
+
+#Test Accounts:
+#@{Name = "Gary.Blok"; GivenName = "Gary"; Surname = "Blok" ; Description = "ME"; Group = @("Local Admins Workstations","Domain Admins", "CM Full Admins","CM App Deploy Users")}
+#@{Name = "Mark.Godfrey"; GivenName = "Mark"; Surname = "Godfrey" ; Description = "Friend"; Group = @("Local Admins Workstations", "CM Full Admins","CM App Deploy Users")}
+#@{Name = "David.Segura"; GivenName = "David"; Surname = "Segura" ; Description = "Friend"; Group = @("Local Admins Workstations","CM App Deploy Users")}
+#@{Name = "Nathan.Ziehnert"; GivenName = "Nathan"; Surname = "Ziehnert" ; Description = "Friend"; Group = @("Domain Admins", "CM Full Admins","CM App Deploy Users")}
+#@{Name = "Troy.Martin"; GivenName = "Troy"; Surname = "Martin" ; Description = "Friend"; Group = @("CM Full Admins","CM App Deploy Users")}
+#@{Name = "Mike.Terrill"; GivenName = "Mike"; Surname = "Terrill" ; Description = "Friend"; Group = @("Local Admins Workstations","CM Full Admins","CM App Deploy Users")}
 )
 
 #Notes:  LogonWorkstation Null is set so those accounts can't be used to logon to any workstations.... unless you actually named a machine "Null"
@@ -145,20 +161,37 @@ Set-Acl -Path $OrganizationalUnit -AclObject $ACL
 #endregion
 
 #region Create GPOs
-New-GPO -Name "All Servers"
-New-GPLink -Name "All Servers" -Target $ServersOU.DistinguishedName
+$GPOName = "All Servers"
+if (!(Get-GPO -Name $GPOName)){
+    New-GPO -Name $GPOName
+    New-GPLink -Name $GPOName -Target $ServersOU.DistinguishedName
+}
+else {
+    Write-Host "GPO $GPOName already Exist" -ForegroundColor Yellow
+}
 
-New-GPO -Name "ConfigMgr Servers"
-New-GPLink -Name "ConfigMgr Servers" -Target $ServersOU.DistinguishedName
-Set-GPPermission -Name "ConfigMgr Servers" -PermissionLevel GpoApply -TargetName "CM Servers" -TargetType Group
-Set-GPPermission -Name "ConfigMgr Servers" -PermissionLevel GpoRead -TargetName "Authenticated Users" -TargetType Group -Replace
+$GPOName = "ConfigMgr Servers"
+if (!(Get-GPO -Name $GPOName)){
+    New-GPO -Name $GPOName
+    New-GPLink -Name $GPOName -Target $ServersOU.DistinguishedName
+    Set-GPPermission -Name $GPOName -PermissionLevel GpoApply -TargetName "CM Servers" -TargetType Group
+    Set-GPPermission -Name $GPOName -PermissionLevel GpoRead -TargetName "Authenticated Users" -TargetType Group -Replace
+}
+else {
+    Write-Host "GPO $GPOName already Exist" -ForegroundColor Yellow
+}
 
-$RootRootOU = $RootOU.DistinguishedName.Replace("OU=$company,","")
-New-GPO -Name "Domain Machines"
-New-GPLink -Name "Domain Machines" -Target $RootRootOU
-Set-GPPermission -Name "Domain Machines" -PermissionLevel GpoRead -TargetName "Authenticated Users" -TargetType Group -Replace
-Set-GPPermission -Name "Domain Machines" -PermissionLevel GpoApply -TargetName "Domain Computers" -TargetType Group
-
+$GPOName = "Domain Machines"
+if (!(Get-GPO -Name $GPOName)){
+    $RootRootOU = $RootOU.DistinguishedName.Replace("OU=$company,","")
+    New-GPO -Name $GPOName
+    New-GPLink -Name $GPOName -Target $RootRootOU
+    Set-GPPermission -Name $GPOName -PermissionLevel GpoRead -TargetName "Authenticated Users" -TargetType Group -Replace
+    Set-GPPermission -Name $GPOName -PermissionLevel GpoApply -TargetName "Domain Computers" -TargetType Group
+}
+else {
+    Write-Host "GPO $GPOName already Exist" -ForegroundColor Yellow
+}
 
 <#Building out GPOs... now Plan to Export, then ZIP, and allow others to import them to save time.. will be hosted on GitHub
 Backup-GPO -Name "All Servers" -Path "c:\GPOBackups"
