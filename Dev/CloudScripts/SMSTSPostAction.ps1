18654524a62ec6f160464d200e27ac0fec9b7547
gwblok <garywblok@gmail.com>
2024-08-26 20:01:48 -0500

    Create SMSTSPostAction.ps1

---
+++ Dev/CloudScripts/SMSTSPostAction.ps1
@@ -0,0 +1,12 @@
+
+Start-Transcript -Path C:\windows\CCM\logs\smstspostaction.txt
+
+iex (irm functions.garytown.com)
+Install-Module -Name OSD
+Install-ModuleHPCMSL
+Set-ThisPC
+Set-TimeZoneFromIP
+Start-WindowsUpdate
+Invoke-UpdateScanMethodMSStore
+
+Stop-Transcript
