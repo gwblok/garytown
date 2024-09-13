456e7681a0082f3b581afb9cd17ce85ee5449cda
gwblok <garywblok@gmail.com>
2024-09-13 14:07:50 -0500

    Update Set-BIOSSettings.ps1

--- hardware/Lenovo/Set-BIOSSettings.ps1
+++ hardware/Lenovo/Set-BIOSSettings.ps1
@@ -6,6 +6,12 @@ Lenovo BIOS Settings return a really ugly dataset, so one of the first things th
 This is still v1.0, I'll probably add more write-output and enabel transcription for the logging.  
 I still need to add BIOS Password Tests, some of the plumbing is there, but not the checks... 
 
+
+Additional References: 
+https://www.configjon.com/lenovo-bios-settings-management/
+https://docs.lenovocdrt.com/ref/bios/sdbm/#wmi-in-system-deployment-boot-mode
+
+
 #>
 $BIOSPass = 'P@ssw0rd'
 
@@ -42,7 +48,26 @@ $PasswordSet = Get-CimInstance -Namespace root\wmi -ClassName Lenovo_SetBiosPass
 #Test Setting
 #$Interface | Invoke-CimMethod -MethodName SetBIOSSetting -Arguments @{ parameter = "WirelessLANPXE,Disabled,$BIOSPass,ascii,us" }
 
-
+switch($PasswordSettings.PasswordState)
+{
+	{$_ -eq 0}
+	{
+		Write-Output "No passwords are currently set"
+	}
+	{($_ -eq 2) -or ($_ -eq 3) -or ($_ -eq 6) -or ($_ -eq 7) -or ($_ -eq 66) -or ($_ -eq 67) -or ($_ -eq 70) -or ($_-eq 71)}
+	{
+		$SvpSet = $true
+		Write-Output "The supervisor password is set"
+	}
+	{($_ -eq 64) -or ($_ -eq 65) -or ($_ -eq 66) -or ($_ -eq 67) -or ($_ -eq 68) -or ($_ -eq 69) -or ($_ -eq 70) -or ($_-eq 71)}
+	{
+		$SmpSet = $true
+		Write-Output  "The system management password is set"
+	}
+	default
+	{
+	}
+}
 
 #List of settings to be configured ============================================================================================
 #==============================================================================================================================
@@ -69,11 +94,21 @@ foreach ($Setting in $Settings){
             Write-Host "Setting already Set Correctly: $SettingValue" -ForegroundColor Green
         }
         else {
+            $SaveRequired = $true
             Write-Host "Updating Current Value of $CurrentSettingValue to $SettingValue" -ForegroundColor Yellow
-            $Interface | Invoke-CimMethod -MethodName SetBIOSSetting -Arguments @{ parameter = "$SettingName,$SettingValue,$BIOSPass,ascii,us" }
+            if ($SvpSet){
+                $Interface | Invoke-CimMethod -MethodName SetBIOSSetting -Arguments @{ parameter = "$SettingName,$SettingValue,$BIOSPass,ascii,us" }
+            }
+            else {
+                $Interface | Invoke-CimMethod -MethodName SetBIOSSetting -Arguments @{ parameter = "$SettingName,$SettingValue" }
+            }
         }
     }
     else {
         Write-Warning "Setting $SettingName not found in BIOS"
     }
+}
+
+if ($SaveRequired -and $SvpSet){
+    $SaveSettings  | Invoke-CimMethod -MethodName SaveBiosSettings -Arguments @{ parameter = "$BIOSPass,ascii,us" }
 }
