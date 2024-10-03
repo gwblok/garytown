5d635a62e436ccf56244a696ddd4e17e1ccd9313
gwblok <garywblok@gmail.com>
2024-10-03 09:10:17 -0500

    Update OOBE_DELL.ps1

--- Dev/CloudScripts/OOBE_DELL.ps1
+++ Dev/CloudScripts/OOBE_DELL.ps1
@@ -16,36 +16,16 @@ if ($Manufacturer -match "Dell"){
     write-output $Manufacturer
     write-output $Model
     write-output $SystemSKUNumber
-    <#
+    
     $DellEnterprise = Test-DCUSupport
     if ($DellEnterprise -eq $true) {
         Write-Host "Running $ScriptName - $ScriptVersion" -ForegroundColor Green
         Write-Host -ForegroundColor Green "Dell System Supports Dell Command Update"
         Write-Host -ForegroundColor Green " Enabling Dell Functions: https://raw.githubusercontent.com/OSDeploy/OSD/master/cloud/modules/devicesdell.psm1"
         Invoke-Expression (Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/OSDeploy/OSD/master/cloud/modules/devicesdell.psm1')
-    }
-    if ($env:SystemDrive -eq 'X:') {
-        $WindowsPhase = 'WinPE'
-    }
-    else {
-        $WindowsPhase = 'Windows'
-    }
-    write-output "Running in Windows Phase: $WindowsPhase"
-
-    #region Windows
-    if ($WindowsPhase -eq 'Windows') {
-        #Load OSD and Azure stuff
-        Write-Host -ForegroundColor Green "[+] Installing Dell Command Update"
-        osdcloud-InstallDCU
-        Write-Host -ForegroundColor Green "[+] Running Dell Command Update Drivers"
-        osdcloud-RunDCU -UpdateType driver
-        Write-Host -ForegroundColor Green "[+] Running Dell Command Update BIOS"
-        osdcloud-RunDCU -UpdateType bios
-        Write-Host -ForegroundColor Green "[+] Setting Dell Command Update to Auto Update"
-        osdcloud-DCUAutoUpdate
-    }
-    #endregion
-    #>
-    
-    
+        Write-Host -ForegroundColor Green "[+] Funciton: osdcloud-InstallDCU"
+        Write-Host -ForegroundColor Green "[+] Function: osdcloud-RunDCU"
+        Write-Host -ForegroundColor Green "[+] Function: osdcloud-RunDCU"
+        
+    } 
 }
