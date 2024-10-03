493951a5734d222b7c2cfb7a1ad45ed4dfdceded
gwblok <garywblok@gmail.com>
2024-10-03 09:49:47 -0500

    Update OOBE_DELL.ps1

--- Dev/CloudScripts/OOBE_DELL.ps1
+++ Dev/CloudScripts/OOBE_DELL.ps1
@@ -8,24 +8,22 @@ $Manufacturer = (Get-CimInstance -Class:Win32_ComputerSystem).Manufacturer
 $Model = (Get-CimInstance -Class:Win32_ComputerSystem).Model
 $SystemSKUNumber = (Get-CimInstance -ClassName Win32_ComputerSystem).SystemSKUNumber
 
-Invoke-Expression (Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/gwblok/garytown/master/hardware/Dell/CommandUpdate/CMSL/Dell-CMSL.ps1')
 
 if ($Manufacturer -match "Dell"){
+    Invoke-Expression (Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/gwblok/garytown/master/hardware/Dell/CommandUpdate/CMSL/Dell-CMSL.ps1')
+
     $Manufacturer = "Dell"
-    
     write-output "Manufacturer:    $Manufacturer"
     write-output "Model:           $Model"
     write-output "SystemSKUNumber: $SystemSKUNumber"
     
-    $DellEnterprise = Test-DCUSupport
-    if ($DellEnterprise -eq $true) {
-        Write-Host "Running $ScriptName - $ScriptVersion" -ForegroundColor Green
-        Write-Host -ForegroundColor Green "Dell System Supports Dell Command Update"
-        Write-Host -ForegroundColor Green " Enabling Dell Functions: https://raw.githubusercontent.com/OSDeploy/OSD/master/cloud/modules/devicesdell.psm1"
-        Invoke-Expression (Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/OSDeploy/OSD/master/cloud/modules/devicesdell.psm1')
-        Write-Host -ForegroundColor Green "[+] Funciton: osdcloud-InstallDCU"
-        Write-Host -ForegroundColor Green "[+] Function: osdcloud-RunDCU"
-        Write-Host -ForegroundColor Green "[+] Function: osdcloud-RunDCU"
-        
-    } 
+    Write-Host -ForegroundColor Green "[+] Function: Get-DellSupportedModels"
+    Write-Host -ForegroundColor Green "[+] Function: Get-DCUVersion"
+    Write-Host -ForegroundColor Green "[+] Function: Get-DCUInstallDetails"
+    Write-Host -ForegroundColor Green "[+] Function: Get-DCUExitInfo"
+    Write-Host -ForegroundColor Green "[+] Function: Install-DCU"
+    Write-Host -ForegroundColor Green "[+] Function: Invoke-DCU"
+    Write-Host -ForegroundColor Green "[+] Function: Get-DCUUpdateList"
+    Write-Host -ForegroundColor Green "[+] Function: Get-DellDeviceDetails"
+    
 }
