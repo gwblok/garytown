f3a9fd65b6ec9b20a9bb3a2376fd30df5cc0e661
Gary Blok <gary@thebloks.com>
2024-10-07 12:58:39 -0500

    24.10.7.1

--- Intune/IntuneWinCreator.ps1
+++ Intune/IntuneWinCreator.ps1
@@ -11,8 +11,9 @@ $SourceAppPathRoot = "$IntuneAppRootPath\Sources"
 #++++++++++++++++++++++++++
 #!!!Change These!!!
 $CustomAppNameManf = "2Pint"
-$SourceAppPath = "$SourceAppPathRoot\$CustomAppNameManf\Agent\2.10.10714.1630"
-
+$SourceAppPath = "$SourceAppPathRoot\$CustomAppNameManf\Agent\"
+$LatestSourceAppPath = Get-ChildItem -Path $SourceAppPath | Where-Object {$_.Attributes -eq "Directory"} | sort-object -Property LastWriteTime -Descending | Select-Object -First 1
+$SourceAppPath = ($LatestSourceAppPath).FullName
 #++++++++++++++++++++++++++
 
 $OutputAppPath = $SourceAppPath.Replace("Sources","Output")
@@ -30,19 +31,31 @@ if (!(Test-Path -Path $SourceAppPath)){
     Write-Host "Created $SourceAppPath" -ForegroundColor Green
     Write-Host "Place your SOURCE HERE!!!" -ForegroundColor Red
 }
+else {
+    Write-Host "Using Source: $SourceAppPath" -ForegroundColor Green
+}
 
 if (!(Test-Path -Path $OutputAppPath)){
     New-item -Path $OutputAppPath -ItemType Directory -Force | Out-Null
     Write-Host "Created $OutputAppPath" -ForegroundColor Green
+}
+else {
+    Write-Host "Using Output: $OutputAppPath" -ForegroundColor Green
 }
 
 if (!(Test-Path -Path $IntuneUtilFolderPath)){
     New-item -Path $IntuneUtilFolderPath -ItemType Directory -Force | Out-Null
     Write-Host "Created $IntuneUtilFolderPath" -ForegroundColor Green
 }
+else {
+    Write-Host "Using IntuneUtilFolder: $IntuneUtilFolderPath" -ForegroundColor Green
+}
 if (!(Test-Path -Path $IntuneUtilPath)){
     Invoke-WebRequest -UseBasicParsing -Uri $IntuneUtilURL -OutFile $IntuneUtilPath
     Write-Host "Downloaded IntuneWinAppUtil.exe to $IntuneUtilPath" -ForegroundColor Green
+}
+else{
+    Write-Host "Using IntuneWinAppUtil: $IntuneUtilPath" -ForegroundColor Green
 }
 
 
@@ -52,6 +65,7 @@ if (!($SetupEXE)){$SetupEXE = Get-ChildItem -Path $App.FullName -Filter *.msi}
 $SetupFolder = $App.FullName
 #$CreateIntuneApp = Start-Process -FilePath $IntuneUtilPath -ArgumentList "-c $SetupFolder -s $SetupEXEPath -o $OutPutPath -q" -Wait -PassThru
 Write-Host "Starting Intune Package Creation" -ForegroundColor Green
-& $IntuneUtilPath -c $SetupFolder -s $SetupEXE -o $OutputAppPath -q
+write-Host "& $IntuneUtilPath -c $SetupFolder -s $($SetupEXE.FullName) -o $OutputAppPath -q"
+& $IntuneUtilPath -c $SetupFolder -s $SetupEXE.FullName -o $OutputAppPath -q
 
 Write-Host "Finished App for Intune: $OutputAppPath" -ForegroundColor Green
