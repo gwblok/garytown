06ab6b1ef64166e4af123847357d4e9556ee0991
gwblok <garywblok@gmail.com>
2024-10-11 13:38:55 -0500

    Update TaskSequence.ps1

--- Dev/CloudScripts/TaskSequence.ps1
+++ Dev/CloudScripts/TaskSequence.ps1
@@ -1,1 +1,19 @@
 #Stuff to make available in a Task Sequence
+
+Write-Host -ForegroundColor Cyan "Functions for ConfigMgr Task Sequences"
+Write-Host -ForegroundColor Green "[+] Function Confirm-TSEnvironmentSetup"
+Write-Host -ForegroundColor Green "[+] Function Confirm-TSProgressUISetup"
+Write-Host -ForegroundColor Green "[+] Function Get-TSVariables"
+Write-Host -ForegroundColor Green "[+] Function Get-TSValue"
+Write-Host -ForegroundColor Green "[+] Function Get-TSAllValues"
+Write-Host -ForegroundColor Green "[+] Function Set-TSVariable"
+Write-Host -ForegroundColor Green "[+] Function Close-TSProgressDialog"
+Write-Host -ForegroundColor Green "[+] Function Show-TSActionProgress"
+Write-Host -ForegroundColor Green "[+] Function Show-TSProgress"
+Write-Host -ForegroundColor Green "[+] Function Show-TSErrorDialog"
+Write-Host -ForegroundColor Green "[+] Function Show-TSMessage"
+Write-Host -ForegroundColor Green "[+] Function Show-TSRebootDialog"
+Write-Host -ForegroundColor Green "[+] Function Show-TSSwapMediaDialog"
+
+iex (irm 'https://raw.githubusercontent.com/sombrerosheep/TaskSequenceModule/refs/heads/master/SCCM-TSEnvironment.psm1')
+
