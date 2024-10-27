ff5c9a49753deb98c03f72cae5fa46b9fb26d856
gwblok <garywblok@gmail.com>
2024-10-27 17:36:02 -0500

    Update OOBE_DELL.ps1

--- Dev/CloudScripts/OOBE_DELL.ps1
+++ Dev/CloudScripts/OOBE_DELL.ps1
@@ -9,21 +9,20 @@ $Model = (Get-CimInstance -Class:Win32_ComputerSystem).Model
 $SystemSKUNumber = (Get-CimInstance -ClassName Win32_ComputerSystem).SystemSKUNumber
 
 
-if ($Manufacturer -match "Dell"){
-    Invoke-Expression (Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/gwblok/garytown/master/hardware/Dell/CommandUpdate/CMSL/Dell-CMSL.ps1')
 
-    $Manufacturer = "Dell"
-    write-output "Manufacturer:    $Manufacturer"
-    write-output "Model:           $Model"
-    write-output "SystemSKUNumber: $SystemSKUNumber"
-    
-    Write-Host -ForegroundColor Green "[+] Function: Get-DellSupportedModels"
-    Write-Host -ForegroundColor Green "[+] Function: Get-DCUVersion"
-    Write-Host -ForegroundColor Green "[+] Function: Get-DCUInstallDetails"
-    Write-Host -ForegroundColor Green "[+] Function: Get-DCUExitInfo"
-    Write-Host -ForegroundColor Green "[+] Function: Install-DCU"
-    Write-Host -ForegroundColor Green "[+] Function: Invoke-DCU"
-    Write-Host -ForegroundColor Green "[+] Function: Get-DCUUpdateList"
-    Write-Host -ForegroundColor Green "[+] Function: Get-DellDeviceDetails"
-    
-}
+Invoke-Expression (Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/gwblok/garytown/master/hardware/Dell/CommandUpdate/CMSL/Dell-CMSL.ps1')
+
+write-output "Manufacturer:    $Manufacturer"
+write-output "Model:           $Model"
+write-output "SystemSKUNumber: $SystemSKUNumber"
+
+Write-Host -ForegroundColor Green "[+] Function: Get-DellSupportedModels"
+Write-Host -ForegroundColor Green "[+] Function: Get-DCUVersion"
+Write-Host -ForegroundColor Green "[+] Function: Get-DCUInstallDetails"
+Write-Host -ForegroundColor Green "[+] Function: Get-DCUExitInfo"
+Write-Host -ForegroundColor Green "[+] Function: Install-DCU"
+Write-Host -ForegroundColor Green "[+] Function: Invoke-DCU"
+Write-Host -ForegroundColor Green "[+] Function: Get-DCUUpdateList"
+Write-Host -ForegroundColor Green "[+] Function: Get-DellDeviceDetails"
+
+
