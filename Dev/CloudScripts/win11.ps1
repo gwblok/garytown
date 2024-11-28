21a2de79207163dbd4376c5356453e2de2d1dc67
gwblok <garywblok@gmail.com>
2024-11-28 08:51:24 -0600

    Update win11.ps1

--- Dev/CloudScripts/win11.ps1
+++ Dev/CloudScripts/win11.ps1
@@ -22,7 +22,7 @@ $Manufacturer = (Get-CimInstance -ClassName Win32_ComputerSystem).Manufacturer
 $OSVersion = 'Windows 11' #Used to Determine Driver Pack
 $OSReleaseID = '24H2' #Used to Determine Driver Pack
 $OSName = 'Windows 11 24H2 x64'
-$OSEdition = 'Pro'
+$OSEdition = 'Home'
 $OSActivation = 'Retail'
 $OSLanguage = 'en-us'
 
