47ab885f05d66b5a8ca237ce7da37dfcd6273462
gwblok <gary@thebloks.com>
2025-01-27 17:04:21 -0600

    25.1.27.7

--- Dev/CloudScripts/Lenovo.ps1
+++ Dev/CloudScripts/Lenovo.ps1
@@ -5,9 +5,18 @@ iex (irm https://raw.githubusercontent.com/gwblok/garytown/refs/heads/master/har
 Write-Host -ForegroundColor Green "[+] Import-ModuleLenovoCMSL (2.1.0)"
 Write-Host -ForegroundColor Green "[+] Function Install-LenovoVantage"
 Write-Host -ForegroundColor Green "[+] Function Set-LenovoVantage"
+Write-Host -ForegroundColor Green "[+] Function Set-LenovoVantageSU"
+Write-Host -ForegroundColor Green "[+] Function Set-LenovoVantageAutoUpdates"
+Write-Host -ForegroundColor Green "[+] Function Reset-LenovoVantageSettings"
 Write-Host -ForegroundColor Green "[+] Function Install-LenovoSystemUpdater"
 Write-Host -ForegroundColor Green "[+] Function Invoke-LenovoSystemUpdater"
 Write-Host -ForegroundColor Green "[+] Function Set-LenovoBackgroundMonitorDisabled"
+Write-Host ""
+Write-Host -ForegroundColor Magenta "Lenovo Docs for Vantage: https://docs.lenovocdrt.com/guides/cv/commercial_vantage/"
+Write-Host -ForegroundColor Magenta "Lenovo Docs for CMSL: https://docs.lenovocdrt.com/guides/lcsm/lcsm_top/"
+Write-Host ""
+Write-Host "For details about the functions, look at the code on GitHub"
+Write-Host "For details on how Lenovo Commercial Vantage works, see the Lenovo Docs"
 Function Set-LenovoBackgroundMonitorDisabled {
   $LenovoBackgroundTask = Get-ScheduledTask -TaskName "Background monitor" -ErrorAction SilentlyContinue
   if ($LenovoBackgroundTask){
