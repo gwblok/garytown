58041e634ff44c082014ffdce4e01794820898ba
gwblok <gary@thebloks.com>
2025-01-27 10:42:03 -0600

    25.1.27.1

--- Dev/CloudScripts/Lenovo.ps1
+++ Dev/CloudScripts/Lenovo.ps1
@@ -2,6 +2,7 @@ $Manufacturer = (Get-CimInstance -Namespace root/CIMV2 -ClassName Win32_Computer
 Write-Output "Manufacturer = $Manufacturer"
 if ($Manufacturer -match "Lenovo"){
     iex (irm https://raw.githubusercontent.com/OSDeploy/OSD/master/Public/OSDCloudTS/Install-LenovoApps.ps1)
+    Write-Host -ForegroundColor Green "[+] Import-ModuleLenovoCMSL (2.1.0)"
     Write-Host -ForegroundColor Green "[+] Function Install-LenovoVantage"
     Write-Host -ForegroundColor Green "[+] Function Set-LenovoVantage"
     Write-Host -ForegroundColor Green "[+] Function Install-LenovoSystemUpdater"
@@ -15,3 +16,21 @@ if ($Manufacturer -match "Lenovo"){
     }
 }
 
+#Function to install Lenovo CMSL
+Function Import-ModuleLenovoCMSL {
+  
+  $URL = "https://download.lenovo.com/cdrt/tools/Lenovo.Client.Scripting_2.1.0.zip"
+  $Destination = "$env:programdata\CMSL\Lenovo.Client.Scripting_2.1.0.zip"
+  $ExtractedFolder = "$env:programdata\CMSL\Lenovo.Client.Scripting_2.1.0"
+
+  
+  if (!(Test-Path -Path $ExtractedFolder)){
+    New-Item -Path $ExtractedFolder -ItemType Directory | Out-Null
+  }
+  if (!(Test-Path -Path $Destination)){
+    Start-BitsTransfer -Source $URL -Destination $Destination -DisplayName "Lenovo CMSL Download"
+  }
+  Expand-Archive -Path $Destination -DestinationPath $ExtractedFolder -Force
+  $LenovoModule = Get-ChildItem -Path $ExtractedFolder -Recurse | Where-Object { $_.Name -eq "Lenovo.Client.Scripting.psm1" } 
+  Import-Module -Name $LenovoModule.FullName -Force -Verbose
+}
