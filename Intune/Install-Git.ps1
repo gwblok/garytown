3d42e91f4afef825c5c887336892ccfa51f615c4
gwblok <garywblok@gmail.com>
2025-03-19 23:24:34 -0500

    Update Install-Git.ps1

--- Intune/Install-Git.ps1
+++ Intune/Install-Git.ps1
@@ -15,7 +15,10 @@ function Get-InstalledApps
 }
 
 $AppCurrentInstall = Get-InstalledApps | Where-Object {$_.DisplayName -eq "git"}
-
+[version]$AppCurrentInstallVersion = $AppCurrentInstall.DisplayVersion
+if ($null -eq $AppCurrentInstallVersion){
+    [version]$AppCurrentInstallVersion = '0.0.0.1'
+}
 
 # Ensure TLS 1.2 is used for the web request (required for GitHub API)
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
@@ -28,7 +31,7 @@ $release = Invoke-RestMethod -Uri $apiUrl -Method Get
 
 # Extract the version (tag_name) and other useful info
 $version = $release.tag_name
-$NewRelease = ($version.Replace("v","").split(".") | Select-Object -First 3) -join "."
+[version]$NewRelease = ($version.Replace("v","").split(".") | Select-Object -First 3) -join "."
 $publishedDate = $release.published_at
 $downloadUrl = ($release.assets | Where-Object { $_.name -like "*64-bit.exe" }).browser_download_url
 $packageName = ($release.assets | Where-Object { $_.name -like "*64-bit.exe" }).name
@@ -41,7 +44,7 @@ Write-Host "Download URL (64-bit): $downloadUrl"
 
 
 $NewRelease = [version]::new($NewRelease.Major, $NewRelease.Minor, $(if($NewRelease.Build -eq -1){0}else{$NewRelease.Build}), $(if($NewRelease.Revision -eq -1){0}else{$NewRelease.Revision}))
-if ([Version]$NewRelease -match [version]$AppCurrentInstall.DisplayVersion){
+if ([Version]$NewRelease -match [version]$AppCurrentInstallVersion){
     Write-Output "Git already current: $NewRelease"
 }
 else{
