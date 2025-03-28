f0f729e0dfe54f485aa3ab0ca4c7cad2c323e3a1
gwblok <gary@thebloks.com>
2025-03-28 10:20:23 -0500

    25.3.28.10.20

--- BlackLotusKB5025885/IntuneRemediation/KB5025885-PSRemediationScriptStep1.ps1
+++ BlackLotusKB5025885/IntuneRemediation/KB5025885-PSRemediationScriptStep1.ps1
@@ -38,6 +38,10 @@ $Build = $CurrentOSInfo.GetValue('CurrentBuild')
 #April 2024 UBRs
 $AprilPatch = @('19044.4291','19045.4291','22631.3447','22621.3447','22000.2899', '26100.1150','26120.1')
 $MatchedPatch = $AprilPatch | Where-Object {$_ -match $Build}
+if ($null -eq $MatchedPatch){
+    Write-Output "The OS ($Build.$UBR) is not supported for this remediation."
+    exit 5
+}
 [int]$MatchedUBR = $MatchedPatch.split(".")[1]
 
 if ($UBR -ge $MatchedUBR){
@@ -46,14 +50,14 @@ if ($UBR -ge $MatchedUBR){
 else {
     #$OSSupported = $false
     Write-Output "The OS ($Build.$UBR) is not supported for this remediation."
-    exit 4
+    exit 5
 }
 if (Confirm-SecureBootUEFI -ErrorAction SilentlyContinue) {
     #This is required for remediation to work
 }
 else {
     Write-Output "Secure Boot is not enabled."
-    exit 5
+    exit 4
 }
 
 #endregion Applicability
