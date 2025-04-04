d95437819a6c2f3fd8325aeeb8c7b34bbd3dc39e
gwblok <gary@thebloks.com>
2025-04-04 15:28:17 -0500

    25.4.4.15.38

--- HyperV/BuildVMs_BLink.ps1
+++ HyperV/BuildVMs_BLink.ps1
@@ -26,7 +26,8 @@ This script will...
 
 
 # REQUIRED INPUT VARIABLES:
-[int]$DesiredVMs = 2  #The Number of VMs that are going to be created this run.
+[int]$DesiredVMs = 6  #The Number of VMs that are going to be created this run.
+[int]$WaitBetweenNext = 15 #Time in Minutes before starting the next VM - works nice if you don't want to kill your WAN while creating OSDCloud machines.
 
 [int64]$StartingMemory = 4 * 1024 * 1024 * 1024  #4GB
 [int64]$DynamicMemoryLow = 4 * 1024 * 1024 * 1024 #4GB
@@ -371,6 +372,9 @@ else
             }
         else{
             start-vm -Name $VMName
+            if ($WaitBetweenNext){
+                Start-Sleep -Seconds ($WaitBetweenNext * 60)
+            }
         }
 
         <#
