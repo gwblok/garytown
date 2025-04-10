7bacc05b04e6b433c6353006d00168eea52cd775
gwblok <gary@thebloks.com>
2025-04-09 21:08:39 -0500

    25.4.9.21.08

--- hardware/Lenovo/EMPS/Lenovo-MMSDemo.ps1
+++ hardware/Lenovo/EMPS/Lenovo-MMSDemo.ps1
@@ -27,7 +27,31 @@ Write-Host "Get-LnvBiosCode             | "-ForegroundColor Green -NoNewline; Wr
 Write-Host "Get-LnvBiosPasswordsSet     | "-ForegroundColor Green -NoNewline; Write-Host -ForegroundColor Cyan "$(Get-LnvBiosPasswordsSet)"
 Write-Host "Get-LnvBiosUpdateUrl        | "-ForegroundColor Green -NoNewline; Write-Host -ForegroundColor Cyan "$(Get-LnvBiosUpdateUrl)"
 Write-Host "Get-LnvBiosVersion          | "-ForegroundColor Green -NoNewline; Write-Host -ForegroundColor Cyan "$(Get-LnvBiosVersion)"
+Write-Host ""
+Write-Host ""
+Read-Host
 
+Write-Host "Find-LnvModel" -ForegroundColor Green
+Write-Host "Find-LnvModel -MachineType (Get-LnvMachineType)" -ForegroundColor Cyan
+Find-LnvModel -MachineType (Get-LnvMachineType)
+Write-Host "Find-LnvModel -MachineType 11VL" -ForegroundColor Cyan
+Find-LnvModel -MachineType 11VL
+Write-Host ""
+Write-Host ""
+Read-Host
+Write-Host "Find-LnvTool" -ForegroundColor Green
+Write-Host "Find-LnvTool -Tool DockManager" -ForegroundColor Cyan
+Find-LnvTool -Tool DockManager
+Write-Host ""
+Write-Host "Find-LnvTool -Tool SystemUpdate" -ForegroundColor Cyan
+Find-LnvTool -Tool SystemUpdate
+Write-Host ""
+Write-Host "Find-LnvTool -Tool ThinInstaller" -ForegroundColor Cyan
+Find-LnvTool -Tool ThinInstaller
+Write-Host ""
+Write-Host "Find-LnvTool -Tool UpdateRetriever" -ForegroundColor Cyan 
+Find-LnvTool -Tool UpdateRetriever
+Write-Host ""
 Write-Host ""
 Read-Host
 
@@ -43,14 +67,39 @@ Get-LnvBiosInfo
 Write-Host ""
 Read-Host
 
-Write-Host "Get-LnvDriverPack" -ForegroundColor Green
-Get-LnvDriverPack
+Write-Host "Find-LnvDriverPack | " -ForegroundColor Green -NoNewline; Write-Host "Find-LnvDriverPack -MachineType (Get-LnvMachineType) -Latest" -ForegroundColor Cyan
+Find-LnvDriverPack -MachineType (Get-LnvMachineType) -Latest
+Write-Host ""
+Write-Host "$LatestDriverPack = Find-LnvDriverPack -MachineType (Get-LnvMachineType) -Latest" -ForegroundColor Yellow
+$LatestDriverPack = Find-LnvDriverPack -MachineType (Get-LnvMachineType) -Latest
 
 Write-Host ""
 Read-Host
 
-Write-Host "Get-LnvDriverPack" -ForegroundColor Green
-Get-LnvDriverPack
+Write-Host "Get-LnvDriverPack" -ForegroundColor Green 
+Write-Host 'Get-LnvDriverPack -MachineType (Get-LnvMachineType) -WindowsVersion ($LatestDriverPack.os.Replace("win","")) -OSBuildVersion $LatestDriverPack.version  -DownloadPath c:\drivers' -ForegroundColor Cyan
+Read-Host
+Get-LnvDriverPack -MachineType (Get-LnvMachineType) -WindowsVersion ($LatestDriverPack.os.Replace("win","")) -OSBuildVersion $LatestDriverPack.version -DownloadPath c:\drivers
 
 Write-Host ""
 Read-Host
+
+Write-Host "Lets look at some Updates now" -ForegroundColor Magenta
+Write-Host ""
+Write-Host "Find-LnvUpdate" -ForegroundColor Green
+Write-Host "Find-LnvUpdate -MachineType (Get-LnvMachineType)" -ForegroundColor Cyan
+Write-Host ""
+Read-Host
+Find-LnvUpdate -MachineType (Get-LnvMachineType)
+
+Write-Host ""
+Read-Host
+Write-Host "Find-LnvUpdate -MachineType (Get-LnvMachineType) -ListAll" -ForegroundColor Cyan
+Write-Host ""
+Read-Host
+Find-LnvUpdate -MachineType (Get-LnvMachineType) -ListAll
+
+Write-Host ""
+Read-Host
+Write-Host "Now time to Demo the Lenovo Vantage Install and Settings" -ForegroundColor Magenta
+
