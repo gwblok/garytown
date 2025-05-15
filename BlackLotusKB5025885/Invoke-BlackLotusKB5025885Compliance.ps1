22706b8bb5ed1360e5b9cb1cd684b1f6d8d2669b
gwblok <gary@thebloks.com>
2025-05-14 22:01:34 -0500

    25.5.14.22.1

--- BlackLotusKB5025885/Invoke-BlackLotusKB5025885Compliance.ps1
+++ BlackLotusKB5025885/Invoke-BlackLotusKB5025885Compliance.ps1
@@ -3,22 +3,22 @@
 function Invoke-BlackLotusKB5025885Compliance { 
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $false, ParameterSetName = 'StepSelection')]
+        [Parameter(Mandatory = $true, ParameterSetName = 'StepSelection1')]
         [switch] $Step1,
 
-        [Parameter(Mandatory = $false, ParameterSetName = 'StepSelection')]
+        [Parameter(Mandatory = $true, ParameterSetName = 'StepSelection2')]
         [switch] $Step2,
 
-        [Parameter(Mandatory = $false, ParameterSetName = 'StepSelection')]
+        [Parameter(Mandatory = $true, ParameterSetName = 'StepSelection3')]
         [switch] $Step3,
 
-        [Parameter(Mandatory = $false, ParameterSetName = 'StepSelection')]
+        [Parameter(Mandatory = $true, ParameterSetName = 'StepSelection4')]
         [switch] $Step4,
 
-        [Parameter(Mandatory = $false, ParameterSetName = 'StepSelection')]
+        [Parameter(Mandatory = $true, ParameterSetName = 'StepSelection34')]
         [switch] $Step34Combo,
 
-        [Parameter(Mandatory = $false, ParameterSetName = 'StepSelection')]
+        [Parameter(Mandatory = $true, ParameterSetName = 'StepSelectionN')]
         [switch] $NextStep
     ) 
     Function Invoke-Step1 {
@@ -67,7 +67,7 @@ function Invoke-BlackLotusKB5025885Compliance {
     $CurrentOSInfo = Get-Item -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion'
     $Build = $CurrentOSInfo.GetValue('CurrentBuild')
     [int]$UBR = $CurrentOSInfo.GetValue('UBR')
-    $Applicability = $true
+
     #July 2024 UBRs
     $JulyPatch = @('19045.4651','22621.3880','22631.3880','26100.1150','26120.1','26200.1')
     $MatchedPatch = $JulyPatch | Where-Object {$_ -match $Build}
