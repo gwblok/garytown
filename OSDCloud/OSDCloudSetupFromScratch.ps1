7ef3e14516c5f70c4167f7bd320ae7f51841c277
gwblok <gary@thebloks.com>
2025-05-25 15:00:25 -0500

    25.5.25.15.0

--- OSDCloud/OSDCloudSetupFromScratch.ps1
+++ OSDCloud/OSDCloudSetupFromScratch.ps1
@@ -1,15 +1,28 @@
-#Make Sure you're running 24.3.20.1
+<# Gary Blok
+Simple OSDCloud Setup Script for AMD64 (x64) WinPE
+This script will assist in setting up OSDCloud from scratch, including creating a new template, workspace, and USB drive.
+It will name the template & workspace based on the ADK WinPE version installed on the system.
+#>
+
+
+#Make Sure you're running Latest
 Update-Module -name OSD -Force
 
 #Restart PowerShell after OSD has been updated (if it needed to be updated)
 
+#ADK Default Locations
+$ADKAMD64WinPELocation = "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Windows Preinstallation Environment\amd64\en-us\winpe.wim"
+$ADKAMD64WinPEVersion = (Get-WindowsImage -ImagePath $ADKAMD64WinPELocation -Index 1).Version.replace('10.0.','')
+
+$TemplateName = "OSDCloudWinPE-$($ADKAMD64WinPEVersion)"
+
 #Setup WorkSpace Location
 Import-Module -name OSD -force
-$OSDCloudWorkspace = "C:\OSDCloudWinPE"
+$OSDCloudWorkspace = "C:\OSDCloudWinPE-$($ADKAMD64WinPEVersion)"
 [void][System.IO.Directory]::CreateDirectory($OSDCloudWorkspace)
 
 #New Template (After you've updated ADK to lastest Version)
-New-OSDCloudTemplate -Name "OSDCloudWinPE"
+New-OSDCloudTemplate -Name $TemplateName
 
 #New WorkSpace
 New-OSDCloudWorkspace -WorkspacePath $OSDCloudWorkspace
