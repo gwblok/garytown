7647ed7d71f145b1e3a2c3c343acba1e13cb0d88
Gary Blok <gary@thebloks.com>
2025-05-28 14:35:50 -0500

    25.5.28.14.35

--- 2Pint/StifleR/Create-StifleRNetworksInNetworkGroup.ps1
+++ 2Pint/StifleR/Create-StifleRNetworksInNetworkGroup.ps1
@@ -1,21 +1,33 @@
-#Script to create networks in a Network Group in StifleR
+<#Script to create networks in a predefined Network Group in StifleR
+Things for you to change: 
+- GUID(s) for Network Group(s)
+- NetworkID in the Do loop (EX: 192.128.$start.0)
 
+#>
+
+#This assumes you're running this on the StifleR server and that you have the StifleR CIM classes available.
 $serverName = $env:COMPUTERNAME
 
-$NGGuid1 = '97567112-f701-4480-9a20-317b1df2cef1'
-$NGGuid2 = '85f8e19b-15e0-4916-8b5b-3ad1a78fa63f'
+#GUIDs for the Network Groups you want to add networks to.
+$NGGuid1 = 'fd1f1dc9-44be-44d5-ab7d-62710c816d60'
+#$NGGuid2 = '17140f41-7d79-44a9-80f3-e05eba4841e8'
 
-$start = 1
-$end = 32
+#Network range to create networks in the Network Group.
+$start = 0  #Example: 192.128.$start.0
+$end = 31 # Example: 192.128.$end.31
 
+# Loop to create networks in the specified range. - Swap out the NetworkGroupID with the one you want to use.
 do{
     $args = @{
     NetworkGroupID = $NGGuid1;   # <== change to the correct Network Group ID you want to use
-    NetworkID = "172.29.$($start).0";
+    NetworkID = "192.128.$($start).0";
     NetworkMask = "255.255.255.0";
     GatewayMAC = ""
     }
+    Write-Host -ForegroundColor Cyan "Creating Network: $($args.NetworkID) in Network Group: $($args.NetworkGroupID)"
+    #Invoke the CIM method to add the network
     $ret = Invoke-CimMethod -ComputerName $serverName -Namespace root\StifleR -ClassName Networks -MethodName AddNetwork -Arguments $args
     $start += 1
 
 } until ($start -eq $end+1)
+Write-Host "Created networks in Network Group: $NGGuid1 from 192.128.$($start-$end-1).0 to 192.128.$end.0" -ForegroundColor Magenta
