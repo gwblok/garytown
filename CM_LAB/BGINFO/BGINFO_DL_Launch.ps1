af75bdc5b4c0edb20f238e01da070d42165db6cd
Gary Blok <gary@thebloks.com>
2025-06-18 14:23:10 -0500

    BG Info Update

--- CM_LAB/BGINFO/BGINFO_DL_Launch.ps1
+++ CM_LAB/BGINFO/BGINFO_DL_Launch.ps1
@@ -3,6 +3,11 @@
 $FileName = "BGInfo.zip"
 $ExpandPath = "$env:programdata\BGInfo"
 
+if (-not (Test-Path -Path $ExpandPath)) {
+    Write-Output "Creating Directory: $ExpandPath"
+    New-Item -ItemType Directory -Path $ExpandPath -Force
+}
+
 $URL = "https://download.sysinternals.com/files/$FileName"
 Write-Output "Downloading $URL"
 Invoke-WebRequest -UseBasicParsing -Uri $URL -OutFile $env:TEMP\$FileName
@@ -15,10 +20,12 @@ else{Write-Output "Failed Extract"; exit 255}
 
 
 #Upload your own .bgi template file and then download it.
-Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/gwblok/garytown/master/CM_LAB/BGInfo/Server.bgi" -OutFile "$ExpandPath\Server_BGInfo.bgi"
+Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/gwblok/garytown/refs/heads/master/CM_LAB/BGINFO/Server.bgi" -OutFile "$ExpandPath\Server_BGInfo.bgi"
 
 #Download Backgound Image
-Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/gwblok/garytown/master/CM_LAB/BGInfo/2pint-desktop-product-icons-colour-dark-1920x1080.png" -OutFile "$ExpandPath\2pint-desktop-product-icons-colour-dark-1920x1080.png"
+Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/gwblok/garytown/refs/heads/master/CM_LAB/BGINFO/2pint-desktop-product-icons-colour-dark-1920x1080.bmp" -OutFile "$ExpandPath\2pint-desktop-product-icons-colour-dark-1920x1080.png"
+
+
 #Create Process Vars
 $BGinfoPath = "$ExpandPath\bginfo64.exe"
 $BGInfoArgs = "$ExpandPath\Server_BGInfo.bgi /nolicprompt /silent /timer:0"
@@ -27,3 +34,10 @@ $BGInfoArgs = "$ExpandPath\Server_BGInfo.bgi /nolicprompt /silent /timer:0"
 #Start BG Info
 Start-Process -FilePath $BGinfoPath -ArgumentList $BGInfoArgs -PassThru
 
+
+#Create Scheduled Task to run at logon
+$Action = New-ScheduledTaskAction -Execute $BGinfoPath -Argument $BGInfoArgs
+$Trigger = New-ScheduledTaskTrigger -AtLogOn
+$Principal = New-ScheduledTaskPrincipal -GroupId "BUILTIN\Users" -RunLevel Limited
+$Task = New-ScheduledTask -Action $Action -Trigger $Trigger -Principal $Principal -Description "Run BGInfo at user logon"
+Register-ScheduledTask -TaskName "BGInfo" -InputObject $Task -Force
