ef356640ed898b70fa8b88ec7145a458e5223a67
Gary Blok <gary@thebloks.com>
2025-10-02 22:02:49 -0500

    MMS Demo Start

---
+++ hardware/Panasonic/PanasonicMMSDemo.ps1
@@ -0,0 +1,6 @@
+#This is a Demo for Panasonic PowerShell Stuff
+#Gary Blok, 2Pint Software
+
+write-host "Loading Panasonic Demo from panasonic.garytown.com" -ForegroundColor Green
+write-host "--------------------------------------------------" -ForegroundColor DarkGray
+iex (irm panasonic.garytown.com)
