16cb581dc3ec5ebedd637c98ccb2700a10f291a8
gwblok <gary@thebloks.com>
2025-11-28 21:10:28 -0600

    Update for 25H2

--- Dev/CloudScripts/Functions.ps1
+++ Dev/CloudScripts/Functions.ps1
@@ -349,44 +349,10 @@ Function Set-Win11ReqBypassRegValues {
         New-ItemProperty -Path "HKLM:\SYSTEM\Setup\MoSetup" -Name "AllowUpgradesWithUnsupportedTPMOrCPU" -Value 1 -PropertyType DWORD -Force | out-null
     }
 }
-Write-Host -ForegroundColor Green "[+] Function Install-23H2EnablementPackage"
-function Install-23H2EnablementPackage {
-    Function Install-Update {
-        [CmdletBinding()]
-        Param (
-        [Parameter(Mandatory=$true)]
-        $UpdatePath
-        )
-        
-        $scratchdir = 'C:\OSDCloud\Temp'
-        if (!(Test-Path -Path $scratchdir)){
-            new-item -Path $scratchdir | Out-Null
-        }
-        
-        if ($env:SystemDrive -eq "X:"){
-            $Process = "X:\Windows\system32\Dism.exe"
-            $DISMArg = "/Image:C:\ /Add-Package /PackagePath:$UpdatePath /ScratchDir:$scratchdir /Quiet /NoRestart"
-        }
-        else {
-            $Process = "C:\Windows\system32\Dism.exe"
-            $DISMArg = "/Online /Add-Package /PackagePath:$UpdatePath /ScratchDir:$scratchdir /Quiet /NoRestart"
-        }
-        
-        
-        Write-Output "Starting Process of $Process -ArgumentList $DismArg -Wait"
-        $DISM = Start-Process $Process -ArgumentList $DISMArg -Wait -PassThru
-        
-        return $DISM.ExitCode
-    }
-    
-    
-    $23H2EnablementCabURL = "https://raw.githubusercontent.com/gwblok/garytown/master/SoftwareUpdates/Windows11.0-kb5027397-x64.cab"
-    Invoke-WebRequest -UseBasicParsing -Uri $23H2EnablementCabURL -OutFile "$env:TEMP\Windows11.0-kb5027397-x64.cab"
-    
-    if (Test-Path -Path "$env:TEMP\Windows11.0-kb5027397-x64.cab"){
-        Install-Update -UpdatePath "$env:TEMP\Windows11.0-kb5027397-x64.cab"
-    }
-}
+Write-Host -ForegroundColor Green "[+] Function Install-EnablementUpgrade (-version 23H2, 25H2)"
+iex (irm https://raw.githubusercontent.com/gwblok/garytown/refs/heads/master/SoftwareUpdates/Install-Update.ps1)
+
+
 Write-Host -ForegroundColor Green "[+] Function Start-WindowsUpdate"
 iex (irm https://raw.githubusercontent.com/OSDeploy/OSD/master/Public/OSDCloudTS/Start-WindowsUpdate.ps1)
 
