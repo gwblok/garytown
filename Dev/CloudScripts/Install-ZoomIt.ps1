6554bfe715ff50ee83ed76a553b0308461531315
gwblok <gary@thebloks.com>
2026-01-28 20:41:25 -0600

    add process monitor

--- Dev/CloudScripts/Install-ZoomIt.ps1
+++ Dev/CloudScripts/Install-ZoomIt.ps1
@@ -17,3 +17,21 @@ function Install-ZoomIt {
     }
     else{Write-Output "Failed Extract"; exit 255}
 }
+
+function Install-ProcessMonitor {
+    $FileName = "ProcessMonitor.zip"
+    $ExpandPath = "$env:windir\temp"
+    $URL = "https://download.sysinternals.com/files/$FileName"
+    Write-Output "Downloading $URL"
+    Invoke-WebRequest -UseBasicParsing -Uri $URL -OutFile $env:TEMP\$FileName
+    if (Test-Path -Path $env:TEMP\$FileName){Write-Output "Successfully Downloaded"}
+    else{Write-Output "Failed Downloaded"; exit 255}
+    Write-Output "Starting Extraction of $FileName to $ExpandPath"
+    Expand-Archive -Path $env:TEMP\$FileName -DestinationPath $ExpandPath -Force
+    if (Test-Path -Path $ExpandPath\ZoomIt.exe){
+        Write-Output "Successfully Extracted Zip File to temp"
+        Write-Output "Copying ZoomIt.exe to System32"
+        Copy-Item -Path "$ExpandPath\ZoomIt.exe" -Destination "$env:windir\system32\ZoomIt.exe" -Force
+    }
+    else{Write-Output "Failed Extract"; exit 255}
+}
